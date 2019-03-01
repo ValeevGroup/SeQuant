2a70ed624c6c52fb51d20a070817c1361ddc4a67
Eduard Valeyev <eduard@valeyev.net>
2019-03-01 09:34:36 -0500

    SR CC ops can be defined for any rank

--- src/domain/mbpt/sr/sr_op.impl.hpp
+++ src/domain/mbpt/sr/sr_op.impl.hpp
@@ -4,37 +4,45 @@ ExprPtr T_() {
   return Op<N, OpType::t>();
 }
 
-/// makes excitation operator of all ranks up to (and including) @c N
-template <std::size_t N>
-ExprPtr T() {
-  assert(N > 0);
-  ExprPtr result = T_<1>();
-  if (N > 1) result = result + T_<2>();
-  if (N > 2) result = result + T_<3>();
-  if (N > 3) result = result + T_<4>();
-  if (N > 4)
-    throw std::invalid_argument(
-        "sequant::mbpt::sr::so::T<N>: N>4 is not supported");
-  return result;
-}
-
 /// makes lambda deexcitation operator of rank @c N
 template <std::size_t N>
 ExprPtr L_() {
   return Op<N, OpType::l>();
 }
 
+namespace detail {
+
+template <size_t N, OpType Op>
+void op_impl(ExprPtr& result) {
+  static_assert(N > 0);
+  if constexpr (Op == OpType::t)
+    result = result ? result + T_<N>() : T_<N>();
+  else if constexpr (Op == OpType::l)
+    result = result ? result + L_<N>() : L_<N>();
+  else
+    assert(false && "unsupported Op value");
+
+  if constexpr (N > 1) {
+    op_impl<N - 1, Op>(result);
+  }
+}
+}  // namespace detail
+
+/// makes excitation operator of all ranks up to (and including) @c N
+template <std::size_t N>
+ExprPtr T() {
+  static_assert(N > 0);
+  ExprPtr result;
+  detail::op_impl<N, OpType::t>(result);
+  return result;
+}
+
 /// makes excitation operator of all ranks up to (and including) @c N
 template <std::size_t N>
 ExprPtr L() {
-  assert(N > 0);
-  ExprPtr result = L_<1>();
-  if (N > 1) result = result + L_<2>();
-  if (N > 2) result = result + L_<3>();
-  if (N > 3) result = result + L_<4>();
-  if (N > 4)
-    throw std::invalid_argument(
-        "sequant::mbpt::sr::so::L<N>: N>4 is not supported");
+  static_assert(N > 0);
+  ExprPtr result;
+  detail::op_impl<N, OpType::l>(result);
   return result;
 }
 
