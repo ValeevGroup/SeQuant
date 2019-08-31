f3670c2e9b18369e1ffbf6324103aa403ded51ba
Bimal Gaudel <bimalgaudel@example.com>
2019-08-30 20:30:00 -0400

    succinctly populate variables related to Intel tools

--- config.sh
+++ config.sh
@@ -5,12 +5,11 @@
 rm -rf ./build
 
 INTEL_DIR=/opt/intel
-source ${INTEL_DIR}/compilers_and_libraries/linux/mkl/bin/mklvars.sh "intel64"
-source /opt/intel/compilers_and_libraries/linux/tbb/bin/tbbvars.sh "intel64"
+source ${INTEL_DIR}/bin/compilervars.sh -arch intel64 -platform linux
 cmake -Bbuild \
       -DRANGEV3_DIR=/opt/range-v3 \
       -DBTAS_INSTALL_DIR=/opt/BTAS \
       -DBOOST_ROOT=/usr \
       -DCMAKE_PREFIX_PATH=/opt/libint \
-      -DMKL_THREADING=TBB
-
+      -DMKL_THREADING=TBB \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
