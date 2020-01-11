9c9e8689460cf05a60a941d4bd92b029df3ba9a2
nakulteke <nakul@vt.edu>
2020-01-10 21:04:08 -0500

    test for all tensor symmetry

--- tests/unit/test_canonicalize.cpp
+++ tests/unit/test_canonicalize.cpp
@@ -14,20 +14,61 @@ TEST_CASE("Canonicalizer", "[algorithms]") {
   SECTION("nonsymmetric op") {
     auto input = ex<Constant>(0.5) *
                      ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
-                                WstrList{L"p_3", L"p_4"}, Symmetry::symm) *
+                                WstrList{L"p_3", L"p_4"}, Symmetry::nonsymm) *
                      ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
                                 Symmetry::nonsymm) *
                      ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
                                 Symmetry::nonsymm) +
                  ex<Constant>(0.5) *
                      ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                                WstrList{L"p_4", L"p_3"}, Symmetry::symm) *
+                                WstrList{L"p_4", L"p_3"}, Symmetry::nonsymm) *
                      ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
                                 Symmetry::nonsymm) *
                      ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
                                 Symmetry::nonsymm);
     std::wcout << to_latex(input) << std::endl;
     canonicalize(input);
-    std::wcout << to_latex(input) << std::endl;
+    std::wcout << "nonsymmetric: " << to_latex(input) << std::endl;
   }
+
+  SECTION("asymmetric op") {
+    auto input = ex<Constant>(0.5) *
+        ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                   WstrList{L"p_3", L"p_4"}, Symmetry::symm) *
+        ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
+                   Symmetry::nonsymm) *
+        ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
+                   Symmetry::nonsymm) +
+        ex<Constant>(0.5) *
+            ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
+                       WstrList{L"p_4", L"p_3"}, Symmetry::symm) *
+            ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
+                       Symmetry::nonsymm) *
+            ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
+                       Symmetry::nonsymm);
+    // std::wcout << "" << to_latex(input) << std::endl;
+    canonicalize(input);
+    std::wcout << "asymmetric: " << to_latex(input) << std::endl;
+  }
+
+  SECTION("antisymmetric op") {
+    auto input = ex<Constant>(0.5) *
+        ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                   WstrList{L"p_3", L"p_4"}, Symmetry::antisymm) *
+        ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
+                   Symmetry::nonsymm) *
+        ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
+                   Symmetry::nonsymm) +
+        ex<Constant>(0.5) *
+            ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
+                       WstrList{L"p_4", L"p_3"}, Symmetry::antisymm) *
+            ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
+                       Symmetry::nonsymm) *
+            ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
+                       Symmetry::nonsymm);
+    // std::wcout << "" << to_latex(input) << std::endl;
+    canonicalize(input);
+    std::wcout << "nonsymmetric: " << to_latex(input) << std::endl;
+  }
+
 }
