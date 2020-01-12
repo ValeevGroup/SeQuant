9a1fff367ae9c8984c02a6fcf13a501e439d9f4e
Bimal Gaudel <bimalgaudel@gmail.com>
2020-01-12 07:37:01 -0500

    setup ground for evaluation

---
+++ src/SeQuant/domain/evaluate/evalexpr.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Bimal Gaudel on 2020-01-12
+//
+#include <SeQuant/core/expr.hpp>
+
+namespace sequant::evaluate {
+class EvalExpr : public Expr {};
+} // namespace sequant::evaluate
