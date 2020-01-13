bc4ca6e708b6d46133ea3929b7d55923689fdef8
nakulteke <nakul@vt.edu>
2020-01-13 12:19:03 -0500

    Merge branch 'spin_trace' of https://github.com/ValeevGroup/SeQuant2 into spin_trace
    * 'spin_trace' of https://github.com/ValeevGroup/SeQuant2:
      ok, minimal testing of canonization of nonsymm tensors appears to work
      nonsymmetric abstract tensors can be canonicalized also!
    
    # Conflicts:
    #	tests/unit/test_canonicalize.cpp

--- tests/unit/test_canonicalize.cpp
+++ tests/unit/test_canonicalize.cpp
@@ -13,10 +13,10 @@ TEST_CASE("Canonicalizer", "[algorithms]") {
 
   SECTION("Tensors") {
     {
-    auto op = ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
-                         WstrList{L"p_3", L"p_4"}, Symmetry::nonsymm);
-    canonicalize(op);
-    REQUIRE(to_latex(op) == L"{g^{{p_3}{p_4}}_{{p_1}{p_2}}}");
+      auto op = ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                           WstrList{L"p_3", L"p_4"}, Symmetry::nonsymm);
+      canonicalize(op);
+      REQUIRE(to_latex(op) == L"{g^{{p_3}{p_4}}_{{p_1}{p_2}}}");
     }
     {
       auto op = ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
@@ -39,87 +39,74 @@ TEST_CASE("Canonicalizer", "[algorithms]") {
   }
 
   SECTION("sum of products") {
-    auto input = ex<Constant>(0.5) *
-                     ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
-                                WstrList{L"p_3", L"p_4"}, Symmetry::nonsymm, BraKetSymmetry::symm) *
-                     ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
-                                Symmetry::nonsymm) *
-                     ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
-                                Symmetry::nonsymm) +
-                 ex<Constant>(0.5) *
-                     ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                                WstrList{L"p_4", L"p_3"}, Symmetry::nonsymm, BraKetSymmetry::symm) *
-                     ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
-                                Symmetry::nonsymm) *
-                     ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
-                                Symmetry::nonsymm);
-    std::wcout <<  "\nInput: " << to_latex(input) << std::endl;
-    canonicalize(input);
-    std::wcout << "nonsymmetric: " << to_latex(input) << std::endl;
-  }
+    {
+      // CASE 1: Non-symmetric tensors
+      auto input = ex<Constant>(0.5) *
+                       ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                                  WstrList{L"p_3", L"p_4"}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_3"}},
+                                  IndexList{{L"p_1"}}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_4"}},
+                                  IndexList{{L"p_2"}}, Symmetry::nonsymm) +
+                   ex<Constant>(0.5) *
+                       ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
+                                  WstrList{L"p_4", L"p_3"}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_3"}},
+                                  IndexList{{L"p_1"}}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_4"}},
+                                  IndexList{{L"p_2"}}, Symmetry::nonsymm);
+      canonicalize(input);
+      REQUIRE(to_latex(input) ==
+              L"{ "
+              L"\\left({{g^{{p_2}{p_4}}_{{p_1}{p_3}}}{t^{{p_1}}_{{p_2}}}{t^{{p_"
+              L"3}}_{{p_4}}}}\\right) }");
+    }
 
-  SECTION("asymmetric op") {
-    auto input = ex<Constant>(0.5) *
-        ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
-                   WstrList{L"p_3", L"p_4"}, Symmetry::symm, BraKetSymmetry::symm) *
-        ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
-                   Symmetry::nonsymm) *
-        ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
-                   Symmetry::nonsymm) +
-        ex<Constant>(0.5) *
-            ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                       WstrList{L"p_4", L"p_3"}, Symmetry::symm, BraKetSymmetry::symm) *
-            ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
-                       Symmetry::nonsymm) *
-            ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
-                       Symmetry::nonsymm);
-     std::wcout << "\nInput: " << to_latex(input) << std::endl;
-    canonicalize(input);
-    std::wcout << "asymmetric: " << to_latex(input) << std::endl;
-  }
-
-  SECTION("antisymmetric op") {
-
-    // CASE 1
+    // CASE 2: Symmetric tensors
     {
       auto input = ex<Constant>(0.5) *
-          ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
-                     WstrList{L"p_3", L"p_4"}, Symmetry::antisymm, BraKetSymmetry::symm) *
-          ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
-                     Symmetry::nonsymm) *
-          ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
-                     Symmetry::nonsymm) +
+                       ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                                  WstrList{L"p_3", L"p_4"}, Symmetry::symm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_3"}},
+                                  IndexList{{L"p_1"}}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_4"}},
+                                  IndexList{{L"p_2"}}, Symmetry::nonsymm) +
+                   ex<Constant>(0.5) *
+                       ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
+                                  WstrList{L"p_4", L"p_3"}, Symmetry::symm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_3"}},
+                                  IndexList{{L"p_1"}}, Symmetry::nonsymm) *
+                       ex<Tensor>(L"t", IndexList{{L"p_4"}},
+                                  IndexList{{L"p_2"}}, Symmetry::nonsymm);
+      canonicalize(input);
+      REQUIRE(to_latex(input) ==
+              L"{ "
+              L"\\left({{g^{{p_1}{p_4}}_{{p_2}{p_3}}}{t^{{p_2}}_{{p_1}}}{t^{{p_"
+              L"3}}_{{p_4}}}}\\right) }");
+    }
+
+    // Case 3: Anti-symmetric tensors
+    {
+      auto input =
+          ex<Constant>(0.5) *
+              ex<Tensor>(L"g", WstrList{L"p_1", L"p_2"},
+                         WstrList{L"p_3", L"p_4"}, Symmetry::antisymm) *
+              ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
+                         Symmetry::nonsymm) *
+              ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
+                         Symmetry::nonsymm) +
           ex<Constant>(0.5) *
               ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                         WstrList{L"p_4", L"p_3"}, Symmetry::antisymm, BraKetSymmetry::symm) *
+                         WstrList{L"p_4", L"p_3"}, Symmetry::antisymm) *
               ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}},
                          Symmetry::nonsymm) *
               ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}},
                          Symmetry::nonsymm);
-      std::wcout << "\nInput: " << to_latex(input) << std::endl;
       canonicalize(input);
-      // REQUIRE(to_latex(input) == L"{ \\left({{g^{{p_2}{p_4}}_{{p_1}{p_3}}}{t^{{p_1}}_{{p_2}}}{t^{{p_3}}_{{p_4}}}}\\right) }");
-      std::wcout << "antisymmetric: " << to_latex(input) << std::endl;
-    }
-
-    // CASE 2
-    {
-      auto input = ex<Constant>(0.5) *
-          ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                     WstrList{L"p_3", L"p_4"}, Symmetry::antisymm, BraKetSymmetry::symm) *
-          ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}}) *
-          ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}}) +
-          ex<Constant>(0.5) *
-              ex<Tensor>(L"g", WstrList{L"p_2", L"p_1"},
-                         WstrList{L"p_3", L"p_4"}, Symmetry::antisymm, BraKetSymmetry::symm) *
-              ex<Tensor>(L"t", IndexList{{L"p_3"}}, IndexList{{L"p_1"}}) *
-              ex<Tensor>(L"t", IndexList{{L"p_4"}}, IndexList{{L"p_2"}});
-      std::wcout << "\n**Input: " << to_latex(input) << std::endl;
-      canonicalize(input);
-      // REQUIRE(to_latex(input) == L"{ \\left({{g^{{p_2}{p_4}}_{{p_1}{p_3}}}{t^{{p_1}}_{{p_2}}}{t^{{p_3}}_{{p_4}}}}\\right) }");
-      std::wcout << "antisymmetric: " << to_latex(input) << std::endl;
-
+      REQUIRE(to_latex(input) ==
+              L"{ "
+              L"\\left({{g^{{p_1}{p_4}}_{{p_2}{p_3}}}{t^{{p_2}}_{{p_1}}}{t^{{p_"
+              L"3}}_{{p_4}}}}\\right) }");
     }
   }
-
 }
