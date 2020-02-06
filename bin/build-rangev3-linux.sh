cf7bbc7f4338788c939b31b8daf8c8e59aa6c56b
Eduard Valeyev <eduard@valeyev.net>
2020-02-06 16:03:27 -0500

    implemented rangev3 revision tracking

--- bin/build-rangev3-linux.sh
+++ bin/build-rangev3-linux.sh
@@ -3,11 +3,30 @@
 # Exit on error
 set -ev
 
-# Clone Range-V3 unless previous install is cached ... must manually wipe cache on version bump or toolchain update
 export INSTALL_DIR=${INSTALL_PREFIX}/range-v3
+
+# extract the tracked tag of Range-V3
+export RANGEV3_TAG=`grep 'set(SEQUANT_TRACKED_RANGESV3_TAG ' ${TRAVIS_BUILD_DIR}/external/versions.cmake | awk '{print $2}' | sed s/\)//g`
+echo "required Range-V3 revision = ${MADNESS_TAG}"
+
+# make sure installed Range-V3 tag matches the required tag, if not, remove INSTALL_DIR (will cause reinstall)
+# if can't determine the tag, remove and reinstall just in case
+if [ -f "${INSTALL_DIR}/revision.cmake" ]; then
+  export INSTALLED_RANGEV3_TAG=`grep 'set(SEQUANT_INSTALLED_RANGESV3_TAG ' ${INSTALL_DIR}/revision.cmake | awk '{print $2}' | sed s/\)//g`
+  echo "installed Range-V3 revision = ${INSTALLED_RANGEV3_TAG}"
+  if [ "${RANGEV3_TAG}" != "${INSTALLED_RANGEV3_TAG}" ]; then
+    rm -rf "${INSTALL_DIR}"
+  fi
+else
+  rm -rf "${INSTALL_DIR}"
+fi
+
+# Clone Range-V3 unless previous install is cached ... must manually wipe cache on version bump or toolchain update
 if [ ! -d "${INSTALL_DIR}" ]; then
     cd ${INSTALL_PREFIX}
     git clone https://github.com/ericniebler/range-v3.git
+    cd range-v3 && git checkout ${RANGEV3_TAG} && cd ..
+    echo "set(SEQUANT_INSTALLED_RANGESV3_TAG ${RANGEV3_TAG})" > revision.cmake
 else
     echo "Range-V3 already installed ..."
 fi
---
+++ external/versions.cmake
@@ -0,0 +1,9 @@
+# for each dependency track both current and previous id (the variable for the latter must contain PREVIOUS)
+# to be able to auto-update them
+
+set(SEQUANT_TRACKED_BOOST_VERSION 1.67)
+set(SEQUANT_TRACKED_BOOST_PREVIOUS_VERSION 1.67)
+
+set(SEQUANT_TRACKED_RANGESV3_TAG dbdaa247a25a0daa24c68f1286a5693c72ea0006)
+set(SEQUANT_TRACKED_RANGESV3_PREVIOUS_TAG 86633bf5b0528fdce8dcbc3d186af57f2a925774)
+
