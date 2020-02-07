2b3753b37f5ea8a337b2e178932641b973a8039b
Eduard Valeyev <eduard@valeyev.net>
2020-02-07 15:36:17 -0500

    trying download_package for BTAS

---
+++ cmake/modules/FindOrFetchBTAS.cmake
@@ -0,0 +1,14 @@
+find_package(BTAS 1.0.0 QUIET)
+
+if (NOT TARGET BTAS::btas)
+
+  include(DownloadProject)
+  download_project(PROJ                BTAS
+    GIT_REPOSITORY      /Users/evaleev/code/BTAS
+    GIT_TAG             ${SEQUANT_TRACKED_BTAS_TAG}
+    UPDATE_DISCONNECTED 1
+    )
+
+  add_subdirectory(${BTAS_SOURCE_DIR} ${BTAS_BINARY_DIR})
+
+endif(NOT TARGET BTAS::btas)
