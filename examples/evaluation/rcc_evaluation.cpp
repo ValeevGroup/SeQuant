274acd4389145262f50c729d2f229454fe4d92d5
nakulteke <nakul@vt.edu>
2020-05-04 11:32:29 -0400

    rcc_evaluation.cpp test

--- examples/evaluation/rcc_evaluation.cpp
+++ examples/evaluation/rcc_evaluation.cpp
@@ -403,6 +403,9 @@ int main(int argc, char* argv[]) {
     (*t_ov).fill(0.);
     auto t_oovv = std::make_shared<TA::TArrayD>(world, tr_oovv);
     (*t_oovv).fill(0.);
+    auto t_vvoo = std::make_shared<TA::TArrayD>(world, tr_vvoo);
+    (*t_vvoo).fill(0.);
+
 #if CCSDT_eval
     auto t_ooovvv = std::make_shared<TA::TArrayD>(world, tr_ooovvv);
     (*t_ooovvv).fill(0.0);
@@ -536,7 +539,7 @@ int main(int argc, char* argv[]) {
     std::vector<std::shared_ptr<TA::TArrayD>> data_tensors = {Fock_oo, Fock_ov, Fock_vo, Fock_vv,
               G_oooo, G_ooov, G_oovo, G_oovv, G_ovov, G_ovvo, G_vovo, G_voov, G_ovvv, G_vovv, G_vvvv,
               G_vvoo, G_vooo, G_vvvo, G_vvov, G_ovoo,
-              t_ov, t_oovv};
+              t_ov, t_oovv, t_vvoo};
 
     std::vector<sequant::ExprPtr> seq_tensors = {
         std::make_shared<sequant::Tensor>(sequant::Tensor(L"f", {L"i_1"}, {L"i_1"})), // f_oo
@@ -563,7 +566,8 @@ int main(int argc, char* argv[]) {
         std::make_shared<sequant::Tensor>(sequant::Tensor(L"g", {L"i_1",L"a_1"}, {L"i_2",L"i_3"})), //ovoo
 
         std::make_shared<sequant::Tensor>(sequant::Tensor(L"t", {L"i_1",}, {L"a_1"})), //ov
-        std::make_shared<sequant::Tensor>(sequant::Tensor(L"t", {L"i_1",L"i_2"}, {L"a_1",L"a_2"})) //oovv
+        std::make_shared<sequant::Tensor>(sequant::Tensor(L"t", {L"i_1",L"i_2"}, {L"a_1",L"a_2"})), //oovv
+        std::make_shared<sequant::Tensor>(sequant::Tensor(L"t", {L"a_1",L"a_2"}, {L"i_1",L"i_2"})) //vvoo
     };
 
 #if CCSDT_eval
@@ -617,6 +621,7 @@ int main(int argc, char* argv[]) {
 
       auto tile_R2       = R2.find({0,0,0,0}).get();
       auto tile_t_oovv   = (*t_oovv).find({0,0,0,0}).get();
+      auto tile_t_vvoo   = (*t_vvoo).find({0,0,0,0}).get();
 
 #if CCSDT_eval
       auto tile_R3       = R3.find({0,0,0,0,0,0}).get();
