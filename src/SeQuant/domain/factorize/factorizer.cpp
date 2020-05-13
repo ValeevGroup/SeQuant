e1f343ff0b1a3c77993b81fcb9952e60a2d54a15
Bimal Gaudel <bimalgaudel@gmail.com>
2020-05-13 13:58:43 -0400

    Indices of partitions

--- src/SeQuant/domain/factorize/factorizer.cpp
+++ src/SeQuant/domain/factorize/factorizer.cpp
@@ -66,52 +66,72 @@ largest_common_subnet(const ExprPtr& exprA, const ExprPtr& exprB) {
   const auto adj_mat = [&are_connected](const auto& container) {
     // initialize the matrix
     container::svector<container::svector<bool>> mat(
-        container.size(), container::svector<bool>(container.size()));
-    auto aa = 0;
-    for (const auto& t1 : container) {
-      auto bb = 0;
-      for (const auto& t2 : container) {
-        mat[aa][bb] = (aa == bb) ? false : are_connected(t1, t2);
-        ++bb;
+        container.size(), container::svector<bool>(container.size(), false));
+    for (auto ii = 0; ii < container.size() - 1; ++ii)
+      for (auto jj = ii + 1; jj < container.size(); ++jj) {
+        mat[ii][jj] = are_connected(container.at(ii), container.at(jj));
+        mat[jj][ii] = mat[ii][jj];
       }
-      ++aa;
-    }
+
     return mat;
   };
-
+  // creating connectivity matrix
   const auto connect_mat = [&are_connected](const auto& container) {
     using hash_type = evaluate::HashType;
     // initialize the matrix
     container::svector<container::svector<hash_type>> mat(
-        container.size(), container::svector<hash_type>(container.size()));
-    auto aa = 0;
-    for (const auto& t1 : container) {
-      auto bb = 0;
-      for (const auto& t2 : container) {
-        auto hvalue = 0;
-        if ((aa != bb) && are_connected(t1, t2))
-          hvalue =
-              evaluate::EvalTree(std::make_shared<Product>(Product{t1, t2}))
-                  .hash_value();
-        mat[aa][bb] = hvalue;
-        ++bb;
+        container.size(), container::svector<hash_type>(container.size(), 0));
+    for (auto ii = 0; ii < container.size() - 1; ++ii)
+      for (auto jj = ii + 1; jj < container.size(); ++jj) {
+        auto prod = std::make_shared<Product>(
+            Product{container.at(ii), container.at(jj)});
+        mat[ii][jj] = evaluate::EvalTree(prod).hash_value();
+        mat[jj][ii] = mat[ii][jj];
       }
-      ++aa;
-    }
+
     return mat;
   };
 
-  auto print_mat = [](const auto& mat, const auto& header,
-                          const auto& sidebar) {
+  // get positions in a container where different kind of tensors begin
+  // container: {f_ov, f_ov, t_oo, t_oovv, g_oovv, g_oovv}
+  // output:    {(0, 2), (2, 3), (3, 4), (4, 6)}
+  const auto parts_indices = [](const auto& container){
+    using pos_type = size_t;
+    using evaluate::EvalTree;// for hashing tensors by their types
+
+    container::svector<pos_type> indices;
+    indices.push_back(0);
+    for (auto ii = 0; ii < container.size(); ) {
+      auto lead_hvalue = EvalTree(container.at(ii)).hash_value();
+      auto jj = ii + 1;
+      for (; jj < container.size(); ++jj) {
+        auto trail_hvalue = EvalTree(container.at(jj)).hash_value();
+        if (lead_hvalue != trail_hvalue) break;
+      }
+      ii = jj;
+      indices.push_back(ii);
+    }
+    container::svector<std::tuple<pos_type, pos_type>>  result;
+    for (auto ii =0; ii < indices.size()-1; ++ii)
+      result.push_back(std::make_tuple(indices.at(ii), indices.at(ii+1)));
+    return result;
+  };
+
+  auto print_vec = [](const auto& container){
+    for (const auto& idx: container)
+      std::wcout << std::boolalpha << "  " << idx;
+    std::wcout << std::endl;
+  };
+
+  auto print_mat = [&print_vec](const auto& mat, const auto& header,
+                      const auto& sidebar) {
     std::wcout << "    ";
     for (const auto& head : header) std::wcout << head->to_latex() << " ";
     std::wcout << "\n";
     auto ii = 0;
     for (const auto& side : sidebar) {
       std::wcout << side->to_latex() << " ";
-      for (const auto& entry : mat[ii])
-        std::wcout << std::boolalpha << " " << entry << " ";
-      std::wcout << "\n";
+      print_vec(mat[ii]);
       ++ii;
     }
   };
