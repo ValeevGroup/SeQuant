f4034f40ad49b845987bed40cd2bc5d5a5fe86e2
Bimal Gaudel <bimalgaudel@gmail.com>
2020-05-14 11:09:48 -0400

    Common expressions from a pair of tensor networks by judiciously looking at appropriate blocks

--- SeQuant/domain/factorize/factorizer.cpp
+++ SeQuant/domain/factorize/factorizer.cpp
@@ -8,6 +8,7 @@ namespace sequant::factorize {
 
 std::tuple<container::svector<size_t>, container::svector<size_t>>
 largest_common_subnet(const ExprPtr& exprA, const ExprPtr& exprB) {
+  using pos_type = size_t;
   // lambda to get the common tensors in container_t1 and container_t2
   auto common_tensors = [](const auto& container_t1, const auto& container_t2) {
     container::set<ExprPtr> common_t1, common_t2;
@@ -92,16 +93,15 @@ largest_common_subnet(const ExprPtr& exprA, const ExprPtr& exprB) {
     return mat;
   };
 
-  // get positions in a container where different kind of tensors begin
+  // get positions in a container where different 'kind' of tensors begin
   // container: {f_ov, f_ov, t_oo, t_oovv, g_oovv, g_oovv}
   // output:    {(0, 2), (2, 3), (3, 4), (4, 6)}
-  const auto parts_indices = [](const auto& container){
-    using pos_type = size_t;
-    using evaluate::EvalTree;// for hashing tensors by their types
+  const auto parts_indices = [](const auto& container) {
+    using evaluate::EvalTree;  // for hashing tensors by their types
 
     container::svector<pos_type> indices;
     indices.push_back(0);
-    for (auto ii = 0; ii < container.size(); ) {
+    for (auto ii = 0; ii < container.size();) {
       auto lead_hvalue = EvalTree(container.at(ii)).hash_value();
       auto jj = ii + 1;
       for (; jj < container.size(); ++jj) {
@@ -111,20 +111,49 @@ largest_common_subnet(const ExprPtr& exprA, const ExprPtr& exprB) {
       ii = jj;
       indices.push_back(ii);
     }
-    container::svector<std::tuple<pos_type, pos_type>>  result;
-    for (auto ii =0; ii < indices.size()-1; ++ii)
-      result.push_back(std::make_tuple(indices.at(ii), indices.at(ii+1)));
+    container::svector<std::tuple<pos_type, pos_type>> result;
+    for (auto ii = 0; ii < indices.size() - 1; ++ii)
+      result.push_back(std::make_tuple(indices.at(ii), indices.at(ii + 1)));
     return result;
   };
 
-  auto print_vec = [](const auto& container){
-    for (const auto& idx: container)
+  // get common exprs in pair of containers of canonical-ordered tensors
+  auto common_exprs = [&connect_mat, &parts_indices](const auto& containerA,
+                                                     const auto& containerB) {
+    auto partsA = parts_indices(containerA);
+    auto partsB = parts_indices(containerB);
+    assert(partsA.size() == partsB.size());
+
+    // auto adjA = adj_mat(containerA);
+    // auto adjB = adj_mat(containerB);
+    auto connectA = connect_mat(containerA);
+    auto connectB = connect_mat(containerB);
+
+    container::svector<std::tuple<pos_type, pos_type>> commonPairA, commonPairB;
+
+    for (auto pidx = 0; pidx < partsA.size(); ++pidx) {
+      auto partA = partsA.at(pidx);
+      auto partB = partsB.at(pidx);
+      for (auto pA = std::get<0>(partA); pA < std::get<1>(partA); ++pA)
+        for (auto ppA = pA + 1; ppA < std::get<1>(partA); ++ppA)
+          for (auto pB = std::get<0>(partB); pB < std::get<1>(partB); ++pB)
+            for (auto ppB = pB + 1; ppB < std::get<1>(partB); ++ppB) {
+              if (connectA[pA][ppA] == connectB[pB][ppB])
+                commonPairA.push_back(std::make_tuple(pA, ppA));
+              commonPairB.push_back(std::make_tuple(pB, ppB));
+            }
+    }
+    return std::make_tuple(commonPairA, commonPairB);
+  };
+
+  auto print_vec = [](const auto& container) {
+    for (const auto& idx : container)
       std::wcout << std::boolalpha << "  " << idx;
     std::wcout << std::endl;
   };
 
   auto print_mat = [&print_vec](const auto& mat, const auto& header,
-                      const auto& sidebar) {
+                                const auto& sidebar) {
     std::wcout << "    ";
     for (const auto& head : header) std::wcout << head->to_latex() << " ";
     std::wcout << "\n";
@@ -136,10 +165,30 @@ largest_common_subnet(const ExprPtr& exprA, const ExprPtr& exprB) {
     }
   };
 
+  std::wcout << "Adjacency matrix of tensors from A\n";
   print_mat(adj_mat(tensorsA), tensorsA, tensorsA);
+
+  std::wcout << "Adjacency matrix of tensors from B\n";
   print_mat(adj_mat(tensorsB), tensorsB, tensorsB);
+
+  std::wcout << "Connectivity matrix of tensors from A\n";
   print_mat(connect_mat(tensorsA), tensorsA, tensorsA);
+
+  std::wcout << "Connectivity matrix of tensors from B\n";
   print_mat(connect_mat(tensorsB), tensorsB, tensorsB);
+
+  std::wcout << "Common expressions from A and B\n";
+  auto [commonExprsA, commonExprsB] = common_exprs(tensorsA, tensorsB);
+  for (auto ii = 0; ii < commonExprsA.size(); ++ii) {
+    auto printPoint = [](const auto& pt) {
+      std::wcout << "(" << std::get<0>(pt) << ", " << std::get<1>(pt) << ")";
+    };
+    std::wcout << "A:  ";
+    printPoint(commonExprsA.at(ii));
+    std::wcout << "    B:  ";
+    printPoint(commonExprsB.at(ii));
+    std::wcout << std::endl;
+  }
 
   return std::make_tuple(container::svector<size_t>(),
                          container::svector<size_t>());
