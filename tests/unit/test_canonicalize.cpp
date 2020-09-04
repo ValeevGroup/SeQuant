6b7148118e2819f21559f034b1d8e69448e7de7e
nakulteke <nakul@vt.edu>
2020-09-04 13:40:08 -0400

    unit test for f*t3 spin-trace result canonicalization

--- tests/unit/test_canonicalize.cpp
+++ tests/unit/test_canonicalize.cpp
@@ -188,5 +188,81 @@ TEST_CASE("Canonicalizer", "[algorithms]") {
       REQUIRE(to_latex(input) ==
                L"{ \\bigl({{g^{{a_3}{i_1}}_{{i_3}{i_4}}}{t^{{i_3}}_{{a_2}}}{t^{{i_4}{i_2}}_{{a_1}{a_3}}}}\\bigr) }");
     }
+
+    { // Case 5: CCSDT R3: S3 * F * T3
+
+      Logger::get_instance().canonicalize = true;
+
+      { // Terms 1 and 6 from spin-traced result
+        auto input =
+            ex<Constant>(-4.) *
+                ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                           WstrList{L"a_1", L"a_2", L"a_3"},
+                           Symmetry::nonsymm) *
+                ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+                ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                           WstrList{L"i_3", L"i_2", L"i_4"},
+                           Symmetry::nonsymm) +
+            ex<Constant>(-4.) *
+                ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                           WstrList{L"a_1", L"a_2", L"a_3"},
+                           Symmetry::nonsymm) *
+                ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+                ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                           WstrList{L"i_2", L"i_4", L"i_3"}, Symmetry::nonsymm);
+        std::wcout << __LINE__ << "L " << to_latex(input) << "\n";
+        canonicalize(input);
+        std::wcout << __LINE__ << "L " << to_latex(input) << "\n\n";
+      }
+
+      {
+        auto term1 =
+            ex<Constant>(-4.) *
+            ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                       WstrList{L"a_1", L"a_2", L"a_3"}, Symmetry::nonsymm) *
+            ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+            ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                       WstrList{L"i_3", L"i_2", L"i_4"}, Symmetry::nonsymm);
+        auto term2 =
+            ex<Constant>(-4.) *
+            ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                       WstrList{L"a_1", L"a_2", L"a_3"}, Symmetry::nonsymm) *
+            ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+            ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                       WstrList{L"i_2", L"i_4", L"i_3"}, Symmetry::nonsymm);
+        std::wcout << __LINE__ << "L Term1: " << to_latex(term1) << "\n";
+        std::wcout << __LINE__ << "L Term2: " << to_latex(term2) << "\n";
+        canonicalize(term1);
+        canonicalize(term2);
+        std::wcout << __LINE__ << "L Term1: " << to_latex(term1) << "\n";
+        std::wcout << __LINE__ << "L Term2: " << to_latex(term2) << "\n";
+        auto sum_of_terms = term1 + term2;
+        std::wcout << __LINE__ << "L " << to_latex(sum_of_terms) << "\n";
+        simplify(sum_of_terms);
+        std::wcout << __LINE__ << "L " << to_latex(sum_of_terms) << "\n\n";
+      }
+
+      { // Terms 2 and 4 from spin-traced result
+        auto input =
+            ex<Constant>(2.) *
+                ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                           WstrList{L"a_1", L"a_2", L"a_3"},
+                           Symmetry::nonsymm) *
+                ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+                ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                           WstrList{L"i_3", L"i_4", L"i_2"},
+                           Symmetry::nonsymm) +
+            ex<Constant>(2.) *
+                ex<Tensor>(L"S", WstrList{L"i_1", L"i_2", L"i_3"},
+                           WstrList{L"a_1", L"a_2", L"a_3"},
+                           Symmetry::nonsymm) *
+                ex<Tensor>(L"f", WstrList{L"i_4"}, WstrList{L"i_1"}) *
+                ex<Tensor>(L"t", WstrList{L"a_1", L"a_2", L"a_3"},
+                           WstrList{L"i_2", L"i_3", L"i_4"}, Symmetry::nonsymm);
+        std::wcout << __LINE__ << "L " << to_latex(input) << "\n";
+        canonicalize(input);
+        std::wcout << __LINE__ << "L " << to_latex(input) << "\n\n";
+      }
+    }
   }
 }
