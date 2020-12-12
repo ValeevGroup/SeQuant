9f516e4031b45acdfc041a7547f0eb233fe9b5b5
Bimal Gaudel <bimalgaudel@gmail.com>
2020-12-12 16:19:54 -0500

    A generic binary expression node and functionalities targeted for it

---
+++ SeQuant/domain/utils/binary_expr.hpp
@@ -0,0 +1,217 @@
+#ifndef SEQUANT_UTILS_BINARY_EXPR_HPP
+#define SEQUANT_UTILS_BINARY_EXPR_HPP
+
+#include <functional>
+#include <memory>
+
+namespace sequant::utils {
+
+template <typename T>
+class binary_expr {
+ private:
+  T data_;
+
+ protected:
+  binary_expr(T&& data) : data_{std::forward<T>(data)} {}
+
+ public:
+  using type = T;
+
+  using node_ptr = std::unique_ptr<binary_expr<T>>;
+
+  virtual ~binary_expr() = default;
+
+  [[nodiscard]] virtual bool leaf() const = 0;
+
+  virtual const node_ptr& left() const = 0;
+
+  virtual const node_ptr& right() const = 0;
+
+  const T& data() const { return data_; }
+};
+
+namespace detail {
+
+template <typename T>
+class binary_expr_internal final : public binary_expr<T> {
+ private:
+  using typename binary_expr<T>::node_ptr;
+
+  node_ptr left_;
+
+  node_ptr right_;
+
+ public:
+  binary_expr_internal(T&& data, node_ptr&& left, node_ptr&& right)
+      : binary_expr<T>(std::forward<T>(data)),
+        left_{std::move(left)},
+        right_{std::move(right)} {}
+
+  binary_expr_internal(const binary_expr_internal<T>&) = delete;
+
+  binary_expr_internal<T>& operator=(const binary_expr_internal<T>&) = delete;
+
+  bool leaf() const override { return false; }
+
+  const node_ptr& left() const override { return left_; }
+
+  const node_ptr& right() const override { return right_; }
+};
+
+template <typename T>
+class binary_expr_leaf final : public binary_expr<T> {
+ private:
+  using typename binary_expr<T>::node_ptr;
+
+ public:
+  binary_expr_leaf(T&& data) : binary_expr<T>(std::forward<T>(data)) {}
+
+  binary_expr_leaf(const binary_expr_leaf<T>&) = delete;
+
+  binary_expr_leaf<T>& operator=(const binary_expr_leaf<T>&) = delete;
+
+  bool leaf() const override { return true; }
+
+  const node_ptr& left() const override {
+    throw std::logic_error("left() called on leaf node");
+  }
+
+  const node_ptr& right() const override {
+    throw std::logic_error("right() called on leaf node");
+  }
+};
+
+}  // namespace detail
+
+template <typename T, typename V>
+bool operator==(const binary_expr<T>& lhs, const binary_expr<V>& rhs) {
+  return lhs.data() == rhs.data() &&
+         ((lhs.leaf() && rhs.leaf()) ||
+          (*lhs.left() == *rhs.left()) && (*lhs.right() == *rhs.right()));
+}
+
+template <typename T>
+typename binary_expr<T>::node_ptr make_binary_expr(T&& data) {
+  return std::make_unique<detail::binary_expr_leaf<T>>(std::forward<T>(data));
+}
+
+template <typename T>
+typename binary_expr<T>::node_ptr make_binary_expr(
+    T&& data, typename binary_expr<T>::node_ptr&& left,
+    typename binary_expr<T>::node_ptr&& right) {
+  return std::make_unique<detail::binary_expr_internal<T>>(
+      std::forward<T>(data), std::move(left), std::move(right));
+}
+
+template <typename T>
+typename binary_expr<T>::node_ptr make_binary_expr(T&& p, T&& l, T&& r) {
+  return make_binary_expr<T>(std::forward<T>(p),
+                             make_binary_expr<T>(std::forward<T>(l)),
+                             make_binary_expr<T>(std::forward<T>(r)));
+}
+
+template <typename T, typename F>
+void visit_inorder_binary_expr(const typename binary_expr<T>::node_ptr& node,
+                               F&& visitor) {
+  static_assert(std::is_invocable_v<F, const T&>,
+                "visitor signature not matched");
+  if (!node) return;  // if nullptr passed explicitly
+
+  if (node->leaf()) {
+    visitor(node->data());
+    return;
+  }
+
+  // visit left node
+  visit_inorder_binary_expr<T, F>(node->left(), std::forward<F>(visitor));
+
+  // visit this node
+  visitor(node->data());
+
+  // visit right node
+  visit_inorder_binary_expr<T, F>(node->right(), std::forward<F>(visitor));
+}
+
+template <typename T, typename F>
+void visit_preorder_binary_expr(const typename binary_expr<T>::node_ptr& node,
+                                F&& visitor) {
+  static_assert(std::is_invocable_v<F, const T&>,
+                "visitor signature not matched");
+  if (!node) return;  // if nullptr passed explicitly
+
+  if (node->leaf()) {
+    visitor(node->data());
+    return;
+  }
+
+  // visit this node
+  visitor(node->data());
+
+  // visit left node
+  visit_preorder_binary_expr<T, F>(node->left(), std::forward<F>(visitor));
+
+  // visit right node
+  visit_preorder_binary_expr<T, F>(node->right(), std::forward<F>(visitor));
+}
+
+template <typename T, typename F>
+void visit_postorder_binary_expr(const typename binary_expr<T>::node_ptr& node,
+                                 F&& visitor) {
+  static_assert(std::is_invocable_v<F, const T&>,
+                "visitor signature not matched");
+  if (node->leaf()) {
+    visitor(node->data());
+    return;
+  }
+  // visit left node
+  visit_postorder_binary_expr<T, F>(node->left(), std::forward<F>(visitor));
+
+  // visit right node
+  visit_postorder_binary_expr<T, F>(node->right(), std::forward<F>(visitor));
+
+  // visit this node
+  visitor(node->data());
+}
+
+template <typename T, typename R, typename F>
+R evaluate_binary_expr(const typename binary_expr<T>::node_ptr& node,
+                       F&& evaluator) {
+  static_assert(std::is_convertible_v<F, std::function<R(const T&)>>,
+                "evaluator signature does not match");
+  static_assert(
+      std::is_convertible_v<
+          F, std::function<R(const typename binary_expr<T>::node_ptr&, R, R)>>,
+      "evaluator signature does not match");
+
+  if (!node) throw std::logic_error("Called evaluate on nullptr");
+
+  if (node->leaf())
+    return std::invoke(std::forward<F>(evaluator), node->data());
+
+  return evaluator(
+      node,
+      evaluate_binary_expr<T, R, F>(node->left(), std::forward<F>(evaluator)),
+      evaluate_binary_expr<T, R, F>(node->right(), std::forward<F>(evaluator)));
+}
+
+template <typename T, typename R, typename F>
+typename binary_expr<R>::node_ptr transform_binary_expr(
+    const typename binary_expr<T>::node_ptr& node, F&& transformer) {
+  static_assert(std::is_convertible_v<F, std::function<R(const T&)>>,
+                "transformer signature does not match");
+  if (!node) return nullptr;
+
+  auto parent_node = make_binary_expr<R>(transformer(node->data()));
+
+  if (node->leaf()) return parent_node;
+
+  return make_binary_expr<R>(transformer(node->data()),
+                             transform_binary_expr<T, R, F>(
+                                 node->left(), std::forward<F>(transformer)),
+                             transform_binary_expr<T, R, F>(
+                                 node->right(), std::forward<F>(transformer)));
+}
+
+}  // namespace sequant::utils
+
+#endif  // SEQUANT_UTILS_BINARY_EXPR_HPP
---
+++ tests/unit/test_binary_expr.cpp
@@ -0,0 +1,149 @@
+#include <SeQuant/domain/utils/binary_expr.hpp>
+#include <iostream>
+
+#include "catch.hpp"
+
+TEST_CASE("TEST BINARY_EXPR_CLASS", "[binary_expr]") {
+  using sequant::utils::binary_expr;
+  using sequant::utils::make_binary_expr;
+  using sequant::utils::detail::binary_expr_internal;
+  using sequant::utils::detail::binary_expr_leaf;
+
+  SECTION("binary_expr constructors") {
+    using namespace std::string_literals;
+    REQUIRE_NOTHROW(binary_expr_internal<size_t>(0, nullptr, nullptr));
+
+    auto node1 = binary_expr_leaf(0);
+    REQUIRE(node1.leaf());
+
+    auto node2 = make_binary_expr(0, make_binary_expr(1), make_binary_expr(2));
+
+    REQUIRE(node2->left() != nullptr);
+    REQUIRE(node2->right() != nullptr);
+    REQUIRE_FALSE(node2->leaf());
+
+    REQUIRE_NOTHROW([&node2] { auto node3 = std::move(node2); });
+
+    REQUIRE_NOTHROW(make_binary_expr(1, 2, 3));
+
+    auto node3 = make_binary_expr("P"s, "L"s, "R"s);
+    REQUIRE(node3->data() == "P"s);
+    REQUIRE(node3->left()->data() == "L"s);
+    REQUIRE(node3->right()->data() == "R"s);
+  }
+
+  SECTION("methods") {
+    using data_type = size_t;
+    auto node1 = make_binary_expr(0, make_binary_expr(1), make_binary_expr(2));
+    REQUIRE(node1->data() == 0);
+    REQUIRE(node1->left()->data() == 1);
+    REQUIRE(node1->right()->data() == 2);
+  }
+
+  SECTION("visitor methods") {
+    using namespace std::literals::string_literals;
+    auto node1 = make_binary_expr("foo"s,  //
+                                  "bar"s,  //
+                                  "bazz"s);
+    // node1 tree:
+    //
+    //       foo
+    //      /   \
+    //   bar    bazz
+    //
+
+    struct {
+      std::vector<std::string> words;
+      void operator()(const std::string& str) { words.emplace_back(str); }
+
+    } visitor;
+
+    sequant::utils::visit_inorder_binary_expr<std::string>(node1, std::ref(visitor));
+    REQUIRE(visitor.words == std::vector<std::string>{"bar", "foo", "bazz"});
+
+    visitor.words.clear();
+    sequant::utils::visit_preorder_binary_expr<std::string>(node1, std::ref(visitor));
+    REQUIRE(visitor.words == std::vector<std::string>{"foo", "bar", "bazz"});
+
+    visitor.words.clear();
+    sequant::utils::visit_postorder_binary_expr<std::string>(node1, std::ref(visitor));
+    REQUIRE(visitor.words == std::vector<std::string>{"bar", "bazz", "foo"});
+  }
+
+  SECTION("evaluator") {
+    auto node = make_binary_expr(2, make_binary_expr(3, 5, 7),
+                                 make_binary_expr(11, 13, 17));
+    // node:
+    //
+    //             2
+    //          /     \
+    //         3       11
+    //       /  \      / \
+    //      5    7   13  17
+
+    // sequant::utils::visit_inorder<int>(node,
+    //                            [](const auto& x) { std::cout << x << " "; });
+    // std::cout << std::endl;
+
+    //
+    // evaluator
+    //
+    struct {
+      double operator()(int x) { return x; }
+
+      // sums left and right result then divides by this node's data
+      double operator()(typename binary_expr<int>::node_ptr const& n, double l,
+                        double r) {
+        return (l + r) / n->data();
+      }
+
+    } evaluator;
+
+    //
+    // expected
+    //                 7.4/(1.1 * 2)
+    //              /       \
+    //            /          \
+    //          /             \
+    //         4.0            3/1.1
+    //       /  \            / \
+    //      5.0    7.0   13.0  17.0
+    //
+
+    REQUIRE(sequant::utils::evaluate_binary_expr<int, double>(
+                node, evaluator) == Approx(7.4 / (1.1 * 2)));
+  }
+
+  SECTION("transformation") {
+    using namespace std::literals::string_literals;
+    auto node1 = make_binary_expr("foo"s, "bar"s, "bazz"s);
+    // node1 tree:
+    //
+    //       foo
+    //      /   \
+    //   bar    bazz
+    //
+
+    struct {
+      size_t operator()(const std::string& str) { return str.size(); }
+    } transformer;
+
+    auto node1_trans =
+        sequant::utils::transform_binary_expr<std::string, size_t>(node1,
+                                                                   transformer);
+
+    // node1_trans tree:
+    //
+    //        3
+    //      /   \
+    //     3     4
+
+    struct {
+      std::vector<size_t> lengths;
+      void operator()(size_t x) { lengths.emplace_back(x); }
+    } visitor;
+
+    sequant::utils::visit_inorder_binary_expr<size_t>(node1_trans, visitor);
+    REQUIRE(visitor.lengths == std::vector<size_t>{3, 3, 4});
+  }
+}
