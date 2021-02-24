31144fb551403a8cbb673d93085cb3dbce35d205
Bimal Gaudel <bimalgaudel@gmail.com>
2021-02-24 10:50:00 -0500

    Delete more obsolote files

--- SeQuant/domain/factorize/factorization_utils.hpp
+++
@@ -1,6 +0,0 @@
-#ifdef SEQUANT_FACTORIZATION_UTILS_HPP
-#define SEQUANT_FACTORIZATION_UTILS_HPP
-namespace sequant::factorization {
-//
-}  // namespace sequant::factorization
-#endif  // SEQUANT_FACTORIZATION_UTILS_HPP
--- SeQuant/domain/factorize/imed_tensor.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Bimal Gaudel on 8/5/20.
-//
-
-#include "imed_tensor.hpp"
