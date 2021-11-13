706eb2c093390947fdba11eb6c379ac70a86597a
Bimal Gaudel <bimalgaudel@gmail.com>
2021-11-13 10:05:35 -0500

    Closed-shell spin-trace with biorthogonal transformation done right from sequant.

--- SeQuant/domain/mbpt/spin.cpp
+++ SeQuant/domain/mbpt/spin.cpp
@@ -2,6 +2,7 @@
 
 #include <SeQuant/core/tensor.hpp>
 #include <SeQuant/core/tensor_network.hpp>
+#include <Eigen/Dense>
 #include <unordered_map>
 
 namespace sequant {
@@ -340,6 +341,106 @@ ExprPtr expand_antisymm(const ExprPtr& expr, bool skip_spinsymm) {
     return std::make_shared<Sum>(sum_result);
   } else
     return nullptr;
+}
+
+ExprPtr biorthogonal_transform(
+    const sequant::ExprPtr& expr, const int n_particles,
+    const std::vector<std::vector<sequant::Index>>& ext_index_groups,
+    double threshold) {
+  // Coefficients
+  std::vector<double> bt_coeff_vec;
+  {
+    using namespace Eigen;
+    // Dimension of permutation matrix is n_particles!
+    int n = std::tgamma(n_particles + 1);
+
+    // Permutation matrix
+    Eigen::Matrix<double, Dynamic, Dynamic> M(n, n);
+    {
+      M.setZero();
+      size_t n_row = 0;
+      container::svector<int, 6> v(n_particles), v1(n_particles);
+      std::iota(v.begin(), v.end(), 0);
+      std::iota(v1.begin(), v1.end(), 0);
+      do {
+        std::vector<double> permutation_vector;  // change to std::vector
+        do {
+          auto cycles = sequant::count_cycles(v1, v);
+          permutation_vector.push_back(std::pow(-2, cycles));
+        } while (std::next_permutation(v.begin(), v.end()));
+        Eigen::VectorXd pv_eig = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(
+            permutation_vector.data(), permutation_vector.size());
+        M.row(n_row) = pv_eig;
+        ++n_row;
+      } while (std::next_permutation(v1.begin(), v1.end()));
+      M *= std::pow(-1, n_particles);
+    }
+
+    // Normalization constant
+    double scalar;
+    {
+      auto nonZero = [&threshold](const double& d) {
+        return abs(d) > threshold;
+      };
+
+      // Solve system of equations
+      SelfAdjointEigenSolver<MatrixXd> eig_solver(M);
+      std::vector<double> eig_vals(eig_solver.eigenvalues().size());
+      VectorXd::Map(&eig_vals[0], eig_solver.eigenvalues().size()) =
+          eig_solver.eigenvalues();
+
+      double non0count =
+          std::count_if(eig_vals.begin(), eig_vals.end(), nonZero);
+      scalar = eig_vals.size() / non0count;
+    }
+
+    // Find Pseudo Inverse, get 1st row only
+    MatrixXd pinv = M.completeOrthogonalDecomposition().pseudoInverse();
+    bt_coeff_vec.resize(pinv.rows());
+    VectorXd::Map(&bt_coeff_vec[0], bt_coeff_vec.size()) = pinv.row(0) * scalar;
+  }
+
+  // Transformation maps
+  std::vector<std::map<Index, Index>> bt_maps;
+  {
+    std::vector<Index> idx_list;
+    for (auto& idx_group : ext_index_groups)
+      idx_list.push_back(*idx_group.begin());
+
+    const std::vector<Index> const_idx_list = idx_list;
+
+    do {
+      std::map<Index, Index> map;
+      auto const_list_ptr = const_idx_list.begin();
+      for (auto& i : idx_list) {
+        map.emplace(std::make_pair(*const_list_ptr, i));
+        const_list_ptr++;
+      }
+      bt_maps.push_back(map);
+    } while (std::next_permutation(idx_list.begin(), idx_list.end()));
+  }
+
+  // Checks if the replacement map is a canonical sequence
+  auto is_canonical = [](const std::map<Index, Index>& idx_map) {
+    bool canonical = true;
+    for (auto&& pair : idx_map)
+      if (pair.first != pair.second) return false;
+    return canonical;
+  };
+
+  // Scale transformed expressions and append
+  Sum bt_expr{};
+  auto coeff_it = bt_coeff_vec.begin();
+  for (auto&& map : bt_maps) {
+    if (is_canonical(map))
+      bt_expr.append(ex<Constant>(*coeff_it) * expr->clone());
+    else
+      bt_expr.append(ex<Constant>(*coeff_it) *
+                     sequant::transform_expression(expr->clone(), map));
+    coeff_it++;
+  }
+  ExprPtr result = std::make_shared<Sum>(bt_expr);
+  return result;
 }
 
 bool has_A_label(const ExprPtr& expr) {
@@ -1020,8 +1121,56 @@ container::vector<container::vector<Index>> external_indices(const ExprPtr& expr
   return ext_index_groups;
 }
 
-ExprPtr closed_shell_CC_spintrace(const ExprPtr& expr){
-  return closed_shell_spintrace(expr, external_indices(expr));
+ExprPtr symmetrize_expr(ExprPtr& expr,
+                        const std::vector<std::vector<sequant::Index>>& ext_index_groups) {
+  std::vector<Index> bra_list, ket_list;
+  for (auto&& idx_group : ext_index_groups) {
+    bra_list.push_back(*idx_group.begin());
+    ket_list.push_back(*(idx_group.begin() + 1));
+  }
+  auto S =
+      sequant::Tensor(L"S", bra_list, ket_list, sequant::Symmetry::nonsymm);
+  return sequant::ex<sequant::Tensor>(S) * expr;
+}
+
+ExprPtr closed_shell_CC_spintrace(const ExprPtr& expr) {
+  container::vector<container::vector<Index>> ext_index_groups;
+
+  Tensor A{};
+  for(auto& prod : *expr){
+    if(prod->is<Product>()){
+      if(prod->as<Product>().factor(0)->as<Tensor>().label() == L"A"){
+        A = prod->as<Product>().factor(0)->as<Tensor>();
+        break;
+      }
+    }
+  }
+
+  auto const nparticle = A.bra_rank();
+  assert(nparticle > 0);
+
+  assert(A.bra().size() == A.ket().size());
+
+  auto b_iter = A.bra().begin();
+  for(auto k : A.ket()){
+    container::vector<Index> pair{k, *b_iter};
+    ext_index_groups.push_back(pair);
+    ++b_iter;
+  }
+  assert(ext_index_groups.size() == A.bra_rank());
+  auto stxpr = closed_shell_spintrace(expr, ext_index_groups);
+  canonicalize(stxpr);
+  // Remove S operator
+  for (auto& term : *stxpr) {
+    if (term->is<Product>())
+      term = remove_tensor_from_product(term->as<Product>(), L"S");
+  }
+  stxpr = biorthogonal_transform(stxpr, nparticle, ext_index_groups);
+  // The symmetrizer operator is required for canonicalizer to give the
+  // correct result
+  stxpr = symmetrize_expr(stxpr, ext_index_groups);
+  simplify(stxpr);
+  return stxpr;
 }
 
 /// Collect all indices from an expression
--- SeQuant/domain/mbpt/spin.hpp
+++ SeQuant/domain/mbpt/spin.hpp
@@ -162,6 +162,20 @@ ExprPtr closed_shell_spintrace(
 /// @return external index groups to be used for spintracing
 container::vector<container::vector<Index>> external_indices(const ExprPtr& expr);
 
+// clang-format off
+/// @brief Perform biorthogonal transformation on an arbitrary
+/// residual equation (http://arxiv.org/abs/1805.00565)
+/// @param expr A spin-traced and canonicalized residual equation
+/// @param n_particles Rank of the amplitude
+/// @param ext_index_groups Indices of the left-projector
+/// @param threshold Parameter for calculating scalar coefficients
+/// @return result A biorthogonally transformed SeQuant expression
+// clang-format on
+ExprPtr biorthogonal_transform(
+    const ExprPtr &expr, const int n_particles,
+    const std::vector<std::vector<sequant::Index>> &ext_index_groups = {{}},
+    double threshold = 1.e-12);
+
 /// @brief Transforms Coupled cluster from spin orbital to spatial orbitals
 /// @detailed The external indices are deduced from Antisymmetrization operator
 /// @param expr ExprPtr with spin orbital indices
@@ -170,6 +184,11 @@ ExprPtr closed_shell_CC_spintrace(const ExprPtr& expr);
 
 /// Collect all indices from an expression
 auto index_list(const ExprPtr& expr);
+
+// Multiply an expression by S operator, which is generated from external
+// indices
+ExprPtr symmetrize_expr(ExprPtr& expr,
+                        const std::vector<std::vector<sequant::Index>>& ext_index_groups);
 
 /// @brief Generates spin expressions to be used for open-shell coupled cluster
 /// @detailed Every spin combination of external indices will have all spin
