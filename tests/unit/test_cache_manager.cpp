62fc43043135e85c3fd16653f9ac28a2938c8353
Bimal Gaudel <bimalgaudel@gmail.com>
2022-03-31 15:05:49 -0400

    Typos and refactor cache_manager unit test.

--- tests/unit/test_cache_manager.cpp
+++ tests/unit/test_cache_manager.cpp
@@ -12,46 +12,50 @@ TEST_CASE("TEST_CACHE_MANAGER", "[cache_manager]") {
   using key_type = manager_type::key_t;
   using count_type = manager_type::count_t;
 
-  auto const n_persistent = 4; // arbitrary
-  auto const n_decaying = 4; // arbitrary
-  // arbitrary keys and vals
-  auto const decaying_keys = std::array<key_type, n_persistent>{100, 110, 200, 210};
+  size_t constexpr n_persistent = 4;  // arbitrary
+  size_t constexpr n_decaying = 4;    // arbitrary
+
+  // arbitrary decaying keys
+  auto const decaying_keys =
+      std::array<key_type, n_decaying>{100, 110, 200, 210};
   // decaying entries repeat more than once
-  auto const decaying_repeats = std::array<count_type, n_persistent>{2, 2, 4, 3};
-  auto const decaying_vals = std::array<data_type, n_persistent>{10, 11, 20, 21};
+  auto const decaying_repeats = std::array<count_type, n_decaying>{2, 2, 4, 3};
+  // arbitrary vals corresponding to decaying keys
+  auto const decaying_vals = std::array<data_type, n_decaying>{10, 11, 20, 21};
 
-  // arbitrary vals and keys not present in decaying_keys
-  auto const persistent_keys = std::array<key_type, n_decaying>{111, 222, 333, 444};
-  auto const persistent_vals = std::array<data_type, n_decaying>{11, 22, 33, 44};
+  // arbitrary persistent vals, and keys not present in decaying_keys
+  auto const persistent_keys =
+      std::array<key_type, n_persistent>{111, 222, 333, 444};
+  auto const persistent_vals =
+      std::array<data_type, n_persistent>{11, 22, 33, 44};
 
-  auto man =
+  auto const man_const =
       manager_type(zip(decaying_keys, decaying_repeats), persistent_keys);
 
-  // filling data
-  for (auto&& [k,v]:
-       zip(concat(decaying_keys, persistent_keys),
-           concat(decaying_vals, persistent_vals))) {
-    // NOTE: man.store() calls man.access() implicitly and
-    // returns a shared_ptr to data
-    // hence, a count of lifetime is lost right here
-    REQUIRE(man.store(k, v));
-  }
-
-  auto const man_copy = man;
-
   SECTION("Construction") {
+    auto const& man = man_const;
+
     REQUIRE(man.cache_map().size() == n_persistent + n_decaying);
-    for (auto&& k: concat(decaying_keys, persistent_keys))
-      REQUIRE(man.cache_map().find(k) != man.cache_map().end());
-    for (auto&& [k, c]: zip(decaying_keys, decaying_repeats))
-      // (c - 1) because a lifetime count is lost by implicit access
-      // during storing
-      REQUIRE(man.cache_map().find(k)->second.life_count() == c-1);
+
+    // verifying the life count of decaying entries
+    for (auto&& [k, c] : zip(decaying_keys, decaying_repeats))
+      REQUIRE(man.cache_map().find(k)->second.life_count() == c);
   }
 
   SECTION("Data Access") {
-    // restore the cache manager in with full lifetimes and data
-    man = man_copy;
+    // need a non-const manager object
+    auto man = man_const;
+    // filling data
+    for (auto&& [k, v] : zip(concat(decaying_keys, persistent_keys),
+                             concat(decaying_vals, persistent_vals))) {
+      // NOTE: man.store() calls man.access() implicitly and
+      // returns a shared_ptr to data
+      // hence, a count of lifetime is lost right here
+      REQUIRE(man.store(k, v));
+    }
+
+    // now accessing decaying entries' data from the cache (c - 1) times
+    // where c is the corresponding entry's lifetime count
     for (auto&& [k, v, r] :
          zip(decaying_keys, decaying_vals, decaying_repeats)) {
       // r - 1: the lifetime count at this point
@@ -65,29 +69,30 @@ TEST_CASE("TEST_CACHE_MANAGER", "[cache_manager]") {
         REQUIRE(iter->second.life_count() == i - 1);
       }
     }
+
     // at this point all the decaying entries have only one lifetime left
     // accessing each decaying entry one more time should release
     // their *data* from the memory
-    for (auto&& k: decaying_keys) {
+    for (auto&& k : decaying_keys) {
       auto iter = man.cache_map().find(k);
       REQUIRE(iter->second.life_count() == 1);
-      REQUIRE(man.access(k).value()); // accessed once. non-null ptr returned
-      REQUIRE_FALSE(man.access(k).value()); // nullptr returned
+      REQUIRE(man.access(k).value());  // accessed once. non-null ptr returned
+      REQUIRE_FALSE(man.access(k).value());  // nullptr returned
       REQUIRE(iter->second.life_count() == 0);
     }
 
     // meanwhile, the persistent entries are all intact
-    for (auto&& [k,v]: zip(persistent_keys, persistent_vals))
+    for (auto&& [k, v] : zip(persistent_keys, persistent_vals))
       REQUIRE(*man.access(k).value() == v);
 
     // now we reset the decaying entries which restores thier lifetimes
     man.reset_decaying();
-    for (auto&& [k,c]: zip(decaying_keys, decaying_repeats))
+    for (auto&& [k, c] : zip(decaying_keys, decaying_repeats))
       REQUIRE(man.cache_map().find(k)->second.life_count() == c);
 
     // now we reset all entries
     man.reset_all();
-    for (auto&& k: concat(decaying_keys, persistent_keys))
-      REQUIRE_FALSE(man.access(k).value()); // nullptr to data returned
+    for (auto&& k : concat(decaying_keys, persistent_keys))
+      REQUIRE_FALSE(man.access(k).value());  // nullptr to data returned
   }
 }
