f66eb720446bcaee720de8f6c42e3a3c80dd27f5
connermasteran <cmasteran1@vt.edu>
2022-04-21 10:50:07 -0400

    small bug in cache manager constructor

--- SeQuant/domain/eval/eval.hpp
+++ SeQuant/domain/eval/eval.hpp
@@ -112,7 +112,7 @@ CacheManager<Tensor_t> make_cache_manager(EvalNode const& node,
   auto less_repeating = [](auto const& pair) { return pair.second < 2; };
   ranges::actions::remove_if(hash_to_counts, less_repeating);
 
-  if (!persistent_leaves) return CacheManager<Tensor_t>{hash_to_counts};
+  if (!persistent_leaves) return CacheManager<Tensor_t>{hash_to_counts,{}};
 
   container::svector<size_t> leaf_hashes{};
   node.visit_leaf([&leaf_hashes](auto const& node) {
