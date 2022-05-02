92921a4e76e00c5aabc8593154ae39f191be1d4e
Bimal Gaudel <bimalgaudel@gmail.com>
2022-05-01 23:12:22 -0400

    Tensor-of-tensor type evaluations fully implemented and tested.

--- SeQuant/domain/eval/eval_ta.hpp
+++ SeQuant/domain/eval/eval_ta.hpp
@@ -3,12 +3,15 @@
 
 #include <SeQuant/domain/eval/eval.hpp>
 
-#include <tiledarray.h>
+#include <TiledArray/expressions/contraction_helpers.h>
 #include <TiledArray/expressions/index_list.h>
-#include <TiledArray/expressions/einsum.h>
+#include <tiledarray.h>
 #include <range/v3/all.hpp>
 
 namespace sequant::eval {
+
+template <typename DistArrayTot, typename DistArray>
+using tot_result_t = std::variant<DistArrayTot, DistArray>;
 
 namespace detail {
 
@@ -29,8 +32,7 @@ Tensor_t eval_inode(EvalNode const& node, Tensor_t const& leval,
          "unsupported intermediate operation");
 
   auto assert_imaginary_zero = [](sequant::Constant const& c) {
-    assert(c.value().imag() == 0 &&
-           "complex scalar unsupported");
+    assert(c.value().imag() == 0 && "complex scalar unsupported");
   };
 
   assert_imaginary_zero(node.left()->scalar());
@@ -55,15 +57,16 @@ Tensor_t eval_inode(EvalNode const& node, Tensor_t const& leval,
   return result;
 }
 
-template <typename Tensor_t>
-Tensor_t eval_inode_tot(EvalNode const& node, Tensor_t const& leval,
-                        Tensor_t const& reval) {
+template <typename DA_tot, typename DA>
+tot_result_t<DA_tot, DA> eval_inode_tot(EvalNode const& node,
+                                        tot_result_t<DA_tot, DA> const& leval,
+                                        tot_result_t<DA_tot, DA> const& reval) {
+  using tot_result_t_ = tot_result_t<DA_tot, DA>;
   assert((node->op() == EvalOp::Sum || node->op() == EvalOp::Prod) &&
          "unsupported intermediate operation");
 
   auto assert_imaginary_zero = [](sequant::Constant const& c) {
-    assert(c.value().imag() == 0 &&
-           "complex scalar unsupported");
+    assert(c.value().imag() == 0 && "complex scalar unsupported");
   };
 
   assert_imaginary_zero(node.left()->scalar());
@@ -75,24 +78,72 @@ Tensor_t eval_inode_tot(EvalNode const& node, Tensor_t const& leval,
   auto const& lannot = node.left()->annot();
   auto const& rannot = node.right()->annot();
 
-  auto result = Tensor_t{};
-  if (node->op() == EvalOp::Prod) {
-    // prod
-    // result(this_annot) = (lscal * rscal) * leval(lannot) * reval(rannot);
-    decltype(result) unscaled = TA::expressions::einsum(leval(lannot),
-                                                        reval(rannot), this_annot);
-    result(this_annot) = (lscal * rscal) * unscaled(this_annot);
-  } else {
-    // sum
-    result(this_annot) = lscal * leval(lannot) + rscal * reval(rannot);
-  }
-  TA::get_default_world().gop.fence();
-  return result;
+  struct {
+    std::string const& lannot;
+    std::string const& rannot;
+    std::string const& this_annot;
+    double lscal;
+    double rscal;
+    tot_result_t_ operator()(DA_tot const& lhs, DA_tot const& rhs) {
+      DA_tot result{};
+      result(this_annot) = lscal * lhs(lannot) + rscal * rhs(rannot);
+      return tot_result_t_{result};
+    }
+    tot_result_t_ operator()(DA const& lhs, DA const& rhs) {
+      DA result{};
+      result(this_annot) = lscal * lhs(lannot) + rscal * rhs(rannot);
+      return tot_result_t_{result};
+    }
+    tot_result_t_ operator()(DA_tot const& lhs, DA const& rhs) {
+      throw std::runtime_error("Attempted tos tensor-of-tensor with tensor!");
+    }
+    tot_result_t_ operator()(DA const& lhs, DA_tot const& rhs) {
+      return operator()(rhs, lhs);
+    }
+  } visitor_for_sum{lannot, rannot, this_annot, lscal, rscal};
+
+  struct {
+    std::string const& lannot;
+    std::string const& rannot;
+    std::string const& this_annot;
+    double lscal;
+    double rscal;
+    tot_result_t_ operator()(DA_tot const& lhs, DA_tot const& rhs) {
+      DA_tot unscaled{}, scaled{};
+      TA::expressions::einsum(unscaled(this_annot), lhs(lannot), rhs(rannot));
+      scaled(this_annot) = (lscal * rscal) * unscaled(this_annot);
+      return tot_result_t_{scaled};
+    }
+    tot_result_t_ operator()(DA_tot const& lhs, DA const& rhs) {
+      return operator()(rhs, lhs);
+    }
+    tot_result_t_ operator()(DA const& lhs, DA_tot const& rhs) {
+      DA_tot unscaled{}, scaled{};
+      TA::expressions::einsum(unscaled(this_annot), lhs(lannot), rhs(rannot));
+      scaled(this_annot) = (lscal * rscal) * unscaled(this_annot);
+      return tot_result_t_{scaled};
+    }
+    tot_result_t_ operator()(DA const& lhs, DA const& rhs) {
+      DA scaled{};
+      scaled(this_annot) = (lscal * rscal) * lhs(lannot) * rhs(rannot);
+      return tot_result_t_{scaled};
+    }
+  } visitor_for_prod{lannot, rannot, this_annot, lscal, rscal};
+
+  if (node->op() == EvalOp::Sum) {
+    auto result = std::visit(visitor_for_sum, leval, reval);
+    TA::get_default_world().gop.fence();
+    return result;
+  } else /* node->op() == EvalOp::Prod */ {
+    auto result = std::visit(visitor_for_prod, leval, reval);
+    TA::get_default_world().gop.fence();
+    return result;
+  }  // else { // unreachable }
 }
 
 template <typename Tensor_t, typename Yielder>
 Tensor_t eval_single_node(EvalNode const& node, Yielder&& leaf_evaluator,
-                          CacheManager<Tensor_t const>& cache_manager) {
+                          CacheManager<Tensor_t>& cache_manager) {
   static_assert(
       std::is_invocable_r_v<Tensor_t, Yielder, sequant::Tensor const&>);
 
@@ -115,11 +166,12 @@ Tensor_t eval_single_node(EvalNode const& node, Yielder&& leaf_evaluator,
                                         cache_manager)));
 }
 
-template <typename Tensor_t, typename Yielder>
-Tensor_t eval_single_node_tot(EvalNode const& node, Yielder&& leaf_evaluator,
-                          CacheManager<Tensor_t const>& cache_manager) {
-  static_assert(
-      std::is_invocable_r_v<Tensor_t, Yielder, sequant::Tensor const&>);
+template <typename DA_tot, typename DA, typename Yielder>
+tot_result_t<DA_tot, DA> eval_single_node_tot(
+    EvalNode const& node, Yielder&& leaf_evaluator,
+    CacheManager<tot_result_t<DA_tot, DA>>& cache_manager) {
+  static_assert(std::is_invocable_r_v<tot_result_t<DA_tot, DA>, Yielder,
+                                      sequant::Tensor const&>);
 
   auto const key = node->hash();
 
@@ -132,12 +184,12 @@ Tensor_t eval_single_node_tot(EvalNode const& node, Yielder&& leaf_evaluator,
                    key,
                    eval_inode_tot(
                        node,
-                       eval_single_node_tot(node.left(),
-                                        std::forward<Yielder>(leaf_evaluator),
-                                        cache_manager),
-                       eval_single_node_tot(node.right(),
-                                        std::forward<Yielder>(leaf_evaluator),
-                                        cache_manager)));
+                       eval_single_node_tot(
+                           node.left(), std::forward<Yielder>(leaf_evaluator),
+                           cache_manager),
+                       eval_single_node_tot(
+                           node.right(), std::forward<Yielder>(leaf_evaluator),
+                           cache_manager)));
 }
 
 }  // namespace detail
@@ -160,7 +212,7 @@ Tensor_t eval_single_node_tot(EvalNode const& node, Yielder&& leaf_evaluator,
 /// \return Tensor_t
 template <typename Tensor_t, typename Iterable, typename Yielder>
 auto eval(EvalNode const& node, Iterable const& target_indx_labels,
-          Yielder&& yielder, CacheManager<Tensor_t const>& man) {
+          Yielder&& yielder, CacheManager<Tensor_t>& man) {
   static_assert(
       std::is_invocable_r_v<Tensor_t, Yielder, sequant::Tensor const&>);
 
@@ -168,9 +220,8 @@ auto eval(EvalNode const& node, Iterable const& target_indx_labels,
       target_indx_labels | ranges::to<container::svector<std::string>>;
   ranges::sort(ti_sorted_input);
   auto ti_sorted_node = node->tensor().const_braket() |
-                        ranges::views::transform([](auto const& idx) {
-                          return idx.ascii_label();
-                        }) |
+                        ranges::views::transform(
+                            [](auto const& idx) { return idx.ascii_label(); }) |
                         ranges::to<container::svector<std::string>>;
   ranges::sort(ti_sorted_node);
 
@@ -189,33 +240,32 @@ auto eval(EvalNode const& node, Iterable const& target_indx_labels,
   return scaled;
 }
 
-template <typename Tensor_t,
-          typename Iterable1,
-          typename Iterable2,
-          typename Yielder,
-          std::enable_if_t<std::is_invocable_r_v<Tensor_t,
-                                                 Yielder,
-                                                 sequant::Tensor const&>,
-                           bool> = true>
-Tensor_t eval_tot(EvalNode const& node,
-          Iterable1 const& outer_indx_labels,
-          Iterable2 const& inner_indx_labels,
-          Yielder&& yielder, CacheManager<Tensor_t const>& man) {
-    auto bpindx_rcvd = TA::expressions::BipartiteIndexList{outer_indx_labels, inner_indx_labels};
-    auto bpindx_exst = TA::expressions::BipartiteIndexList{node->annot()};
-    assert(bpindx_exst.first().is_permutation(bpindx_exst.first())
-            && bpindx_exst.second().is_permutation(bpindx_exst.second())
-            && "Invalid target index labels");
+template <
+    typename DA_tot, typename DA, typename Iterable1, typename Iterable2,
+    typename Yielder,
+    std::enable_if_t<std::is_invocable_r_v<tot_result_t<DA_tot, DA>, Yielder,
+                                           sequant::Tensor const&>,
+                     bool> = true>
+DA_tot eval_tot(EvalNode const& node, Iterable1 const& outer_indx_labels,
+                Iterable2 const& inner_indx_labels, Yielder&& yielder,
+                CacheManager<tot_result_t<DA_tot, DA>>& man) {
+  auto bpindx_rcvd =
+      TA::expressions::BipartiteIndexList{outer_indx_labels, inner_indx_labels};
+  auto bpindx_exst = TA::expressions::BipartiteIndexList{node->annot()};
+  assert(bpindx_exst.first().is_permutation(bpindx_exst.first()) &&
+         bpindx_exst.second().is_permutation(bpindx_exst.second()) &&
+         "Invalid target index labels");
 
-    auto result = detail::eval_single_node_tot(node, std::forward<Yielder>(yielder), man);
+  auto result_ =
+      detail::eval_single_node_tot(node, std::forward<Yielder>(yielder), man);
+  DA_tot result = std::get<DA_tot>(result_);
 
-    auto const lannot = bpindx_rcvd.first().string()
-                        + ";"
-                        + bpindx_rcvd.second().string();
-    auto scaled = decltype(result){};
-    scaled(lannot) = node->scalar().value().real() * result(node->annot());
-    TA::get_default_world().gop.fence();
-    return scaled;
+  auto const lannot = static_cast<std::string>(bpindx_rcvd);
+
+  auto scaled = decltype(result){};
+  scaled(lannot) = node->scalar().value().real() * result(node->annot());
+  TA::get_default_world().gop.fence();
+  return scaled;
 }
 
 ///
@@ -236,7 +286,7 @@ Tensor_t eval_tot(EvalNode const& node,
 /// \return Tensor_t
 template <typename Tensor_t, typename Iterable, typename Yielder>
 auto eval_symm(EvalNode const& node, Iterable const& target_indx_labels,
-               Yielder&& yielder, CacheManager<Tensor_t const>& man) {
+               Yielder&& yielder, CacheManager<Tensor_t>& man) {
   auto result =
       eval(node, target_indx_labels, std::forward<Yielder>(yielder), man);
 
@@ -254,6 +304,39 @@ auto eval_symm(EvalNode const& node, Iterable const& target_indx_labels,
   symmetrize_tensor(result.trange().rank(), sym_impl);
   TA::get_default_world().gop.fence();
   return symm_result;
+}
+
+template <typename DA_tot, typename DA, typename Iterable1, typename Iterable2,
+          typename Yielder>
+auto eval_symm_tot(EvalNode const& node, Iterable1 const& outer_indx_labels,
+                   Iterable2 const& inner_indx_labels, Yielder&& yielder,
+                   CacheManager<tot_result_t<DA_tot, DA>>& man) {
+  assert(node->tensor().bra_rank() == node->tensor().ket_rank() &&
+         "Cannot do particle symmetrization on tensor with unequal bra and ket "
+         "ranks!");
+  size_t const rank = node->tensor().bra_rank();
+  auto const lannot_outer =
+      detail::ords_to_annot(ranges::views::iota(size_t{0}, rank));
+  auto const lannot_inner =
+      detail::ords_to_annot(ranges::views::iota(size_t{rank}, 2 * rank));
+  auto const lannot = lannot_outer + ";" + lannot_inner;
+
+  auto const eval_result = eval_tot(node, outer_indx_labels, inner_indx_labels,
+                                    std::forward<Yielder>(yielder), man);
+  auto result = eval_result;
+  result(lannot) -= eval_result(lannot);  // zero tot with the shape of result
+
+  auto symm_tot_impl = [rank, &result, &lannot,
+                        &eval_result](eval::perm_type const& perm) {
+    auto const rannot =
+        detail::ords_to_annot(perm | ranges::views::take(rank)) + ";" +
+        detail::ords_to_annot(perm | ranges::views::drop(rank));
+    result(lannot) += eval_result(rannot);
+  };
+
+  symmetrize_tensor(rank * 2, symm_tot_impl);
+  TA::get_default_world().gop.fence();
+  return result;
 }
 
 ///
@@ -274,7 +357,7 @@ auto eval_symm(EvalNode const& node, Iterable const& target_indx_labels,
 /// \return Tensor_t
 template <typename Tensor_t, typename Iterable, typename Yielder>
 auto eval_antisymm(EvalNode const& node, Iterable const& target_indx_labels,
-                   Yielder&& yielder, CacheManager<Tensor_t const>& man) {
+                   Yielder&& yielder, CacheManager<Tensor_t>& man) {
   auto result =
       eval(node, target_indx_labels, std::forward<Yielder>(yielder), man);
 
@@ -287,12 +370,53 @@ auto eval_antisymm(EvalNode const& node, Iterable const& target_indx_labels,
 
   auto asym_impl = [&result, &antisymm_result,
                     &lannot](auto const& pwp) {  // pwp = perm with phase
-    antisymm_result(lannot) += pwp.phase * result(detail::ords_to_annot(pwp.perm));
+    antisymm_result(lannot) +=
+        pwp.phase * result(detail::ords_to_annot(pwp.perm));
   };
 
   antisymmetrize_tensor(result.trange().rank(), asym_impl);
   TA::get_default_world().gop.fence();
   return antisymm_result;
+}
+
+template <typename DA_tot, typename DA, typename Iterable1, typename Iterable2,
+          typename Yielder>
+auto eval_antisymm_tot(EvalNode const& node, Iterable1 const& outer_indx_labels,
+                       Iterable2 const& inner_indx_labels, Yielder&& yielder,
+                       CacheManager<tot_result_t<DA_tot, DA>>& man) {
+  auto const eval_result = eval_tot(node, outer_indx_labels, inner_indx_labels,
+                                    std::forward<Yielder>(yielder), man);
+  size_t nouter_idx = ranges::distance(outer_indx_labels);
+  size_t ninner_idx = ranges::distance(inner_indx_labels);
+
+  auto const outer_ords =
+      ranges::views::iota(size_t{0}, nouter_idx) | ranges::to<perm_type>;
+
+  auto const inner_ords =
+      ranges::views::iota(nouter_idx, nouter_idx + ninner_idx) |
+      ranges::to<perm_type>;
+
+  auto const lannot = detail::ords_to_annot(outer_ords) + ";" +
+                      detail::ords_to_annot(inner_ords);
+
+  auto result = eval_result;
+  result(lannot) -=
+      eval_result(lannot);  // balance out extraneous eval_result addition
+
+  auto permute_inner_and_add = [&eval_result, &result, &lannot,
+                                &inner_ords](PermWithPhase const& pwp_outer) {
+    auto perm_vec = inner_ords;
+    detail::permute_ords(perm_vec, [&](PermWithPhase const& pwp_inner) {
+      auto const rannot = detail::ords_to_annot(pwp_outer.perm) + ";" +
+                          detail::ords_to_annot(pwp_inner.perm);
+      result(lannot) +=
+          (pwp_outer.phase * pwp_inner.phase) * eval_result(rannot);
+    });
+  };
+
+  auto outer_perm = outer_ords;
+  detail::permute_ords(outer_perm, permute_inner_and_add);
+  return result;
 }
 
 }  // namespace sequant::eval
--- tests/unit/test_eval_ta.cpp
+++ tests/unit/test_eval_ta.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 
+#include <TiledArray/expressions/contraction_helpers.h>
 #include <tiledarray.h>
 #include <SeQuant/core/parse_expr.hpp>
 #include <SeQuant/core/sequant.hpp>
@@ -52,9 +53,10 @@ class rand_tensor_yield {
            "Unsupported IndexSpace type found while generating tensor.");
 
     auto trange_vec =
-        tnsr.const_braket() | transform([this](auto const& idx) {
+        tnsr.const_braket() |
+        transform([no = nocc_, nv = nvirt_](auto const& idx) {
           return TA::TiledRange1{
-              0, idx.space() == IndexSpace::active_occupied ? nocc_ : nvirt_};
+              0, idx.space() == IndexSpace::active_occupied ? no : nv};
         }) |
         ranges::to_vector;
 
@@ -87,11 +89,103 @@ class rand_tensor_yield {
   ///       otherwise throws assertion error. To avoid that use the other
   ///       overload of operator() that takes sequant::Tensor const&
   Tensor_t const& operator()(std::wstring_view label) const {
-    if (auto&& found = label_to_tnsr_.find(tensor_to_key(label));
-        found != label_to_tnsr_.end())
-      return found->second;
-    else
+    auto&& found = label_to_tnsr_.find(label.data());
+    if (found == label_to_tnsr_.end())
+      found = label_to_tnsr_.find(tensor_to_key(label));
+    if (found == label_to_tnsr_.end())
       throw std::runtime_error{"attempted access of non-existent tensor!"};
+    return found->second;
+  }
+};
+
+template <size_t NOCC, size_t NVIRT>
+class rand_tensor_of_tensor_yield {
+ public:
+  using DA_tot_t = TA::DistArray<TA::Tensor<TA::Tensor<double>>>;
+  using DA_t = TA::TArrayD;
+  using tot_result_t = sequant::eval::tot_result_t<DA_tot_t, DA_t>;
+
+ private:
+  TA::World& world_;
+  rand_tensor_yield<DA_t> rand_da;
+  std::map<std::wstring, tot_result_t> label_to_tnsr_;
+
+  DA_tot_t make_rand_tot(std::string const& annot) const {
+    using ranges::views::transform;
+    auto split_pos = annot.find(';');
+
+    auto outer_lbls_str = annot.substr(0, split_pos);
+    auto inner_lbls_str = annot.substr(split_pos + 1);
+    auto outer_lbls = index_label_list(outer_lbls_str);
+    auto inner_lbls = index_label_list(inner_lbls_str);
+
+    auto make_trange1 = [](std::string const& str) {
+      assert(str[0] == 'i' || str[0] == 'a');
+      return TA::TiledRange1{0, str[0] == 'i' ? NOCC : NVIRT};
+    };
+
+    auto make_ubounds = [](std::string const& str) {
+      assert(str[0] == 'i' || str[0] == 'a');
+      return str[0] == 'i' ? NOCC : NVIRT;
+    };
+
+    auto outer_tr1s = outer_lbls | transform(make_trange1) | ranges::to_vector;
+    auto inner_ubounds =
+        inner_lbls | transform(make_ubounds) | ranges::to_vector;
+    auto inner_lbounds = ranges::views::repeat(size_t{0});
+    auto inner_bounds =
+        ranges::views::zip(inner_lbounds, inner_ubounds) | ranges::to_vector;
+
+    DA_tot_t result{world_,
+                    TA::TiledRange{outer_tr1s.begin(), outer_tr1s.end()}};
+
+    result.init_elements([&inner_bounds, this](auto&&) {
+      auto inner_tensor = TA::Tensor<double>{TA::Range(inner_bounds)};
+      ranges::generate(inner_tensor, []() {
+        return static_cast<double>(std::rand()) / RAND_MAX;
+      });
+      return inner_tensor;
+    });
+
+    assert(result.is_initialized() && "Returning uninitialized tensor!");
+    TA::get_default_world().gop.fence();
+    return result;
+  }
+
+  tot_result_t make_rand_tensor(sequant::Tensor const& tnsr) const {
+    using namespace sequant;
+
+    auto annot = to_eval_node(tnsr.clone())->annot();
+    auto split_pos = annot.find(';');
+
+    return split_pos == std::string::npos
+               ? tot_result_t{rand_da.make_rand_tensor(tnsr)}
+               : tot_result_t{make_rand_tot(annot)};
+  }
+
+ public:
+  rand_tensor_of_tensor_yield(TA::World& world)
+      : world_{world}, rand_da{world, NOCC, NVIRT} {}
+
+  tot_result_t const& operator()(sequant::Tensor const& tnsr) {
+    std::wstring const label = tensor_to_key(tnsr);
+    if (auto&& found = label_to_tnsr_.find(label);
+        found != label_to_tnsr_.end()) {
+      return found->second;
+    } else {
+      auto&& success = label_to_tnsr_.emplace(label, make_rand_tensor(tnsr));
+      assert(success.second && "Couldn't store tensor!");
+      return success.first->second;
+    }
+  }
+
+  tot_result_t const& operator()(std::wstring_view label) const {
+    auto&& found = label_to_tnsr_.find(label.data());
+    if (found == label_to_tnsr_.end())
+      found = label_to_tnsr_.find(tensor_to_key(label));
+    if (found == label_to_tnsr_.end())
+      throw std::runtime_error{"attempted access of non-existent tensor!"};
+    return found->second;
   }
 };
 
@@ -229,5 +323,117 @@ TEST_CASE("TEST_EVAL_USING_TA", "[eval]") {
     man1("0,1,2,3") = 0.5 * man1("0,1,2,3");
 
     REQUIRE(norm(man1) == Approx(norm(eval1)));
+  }
+}
+
+TEST_CASE("TEST_EVAL_TOT_USING_TA", "[eval_tot]") {
+  using ranges::views::transform;
+  using sequant::to_eval_node;
+  using sequant::eval::eval;
+  using sequant::eval::eval_antisymm;
+  using sequant::eval::eval_symm;
+  using TA::TArrayD;
+
+  std::srand(2021);
+
+  sequant::TensorCanonicalizer::register_instance(
+      std::make_shared<sequant::DefaultTensorCanonicalizer>());
+
+  SECTION("Tensor-of-tensor") {
+    using namespace sequant;
+
+    std::wstring_view const pno_mp2_expr =
+        L"g{a_1<i_1,i_2>,a_2<i_1,i_2>;i_1,i_2}"  //
+        " +  f{a_1<i_1,i_2>;a_3<i_1,i_2>}"       //
+        " * t{a_3<i_1,i_2>,a_2<i_1,i_2>;i_1,i_2}"
+        " - f{i_3;i_1}"  //
+        " * t{a_1<i_3,i_2>,a_2<i_3,i_2>;i_3,i_2}"
+        " * O{a_1<i_1,i_2>;a_1<i_3,i_2>}"
+        " * O{a_2<i_1,i_2>;a_2<i_3,i_2>}";
+
+    auto expr = parse_expr(pno_mp2_expr, Symmetry::nonsymm);
+
+    auto& world = TA::get_default_world();
+    auto tensor_yield = rand_tensor_of_tensor_yield<5, 20>{world};
+
+    using tot_result_t = decltype(tensor_yield)::tot_result_t;
+    auto cman = eval::CacheManager<tot_result_t>{{}, {}};
+    using DA_tot = decltype(tensor_yield)::DA_tot_t;
+    using DA = decltype(tensor_yield)::DA_t;
+
+    auto verify_equal_tot_tile = [](auto const& lhs, auto const& rhs) {
+      REQUIRE(lhs.range() == rhs.range());
+      for (auto&& [t1, t2] : ranges::views::zip(lhs, rhs)) {
+        for (auto&& [e1, e2] : ranges::views::zip(t1, t2))
+          if (e1 != 0.0)  // Catch2 flagged (0.0 == Approx(0.0) to be false.
+            REQUIRE(e1 == Approx(e2));
+      }
+    };
+
+    auto const node = to_eval_node(expr);
+
+    auto eval_result = eval::eval_tot(
+        node, std::vector<std::string>{"i_1", "i_2"},
+        std::vector<std::string>{"a_1", "a_2"}, tensor_yield, cman);
+
+    DA_tot man_result{}, temp{}, lhs{}, rhs{};
+
+    // first term evaluated manually
+    temp = std::get<DA_tot>(tensor_yield(L"g{v<o,o>,v<o,o>;o,o}"));
+    man_result("i1,i2;a1,a2") = temp("i1,i2;a1,a2");
+
+    // second term evaluated manually
+    lhs = std::get<DA_tot>(tensor_yield(L"f{v<o,o>;v<o,o>}"));
+    rhs = std::get<DA_tot>(tensor_yield(L"t{v<o,o>,v<o,o>;o,o}"));
+
+    TA::expressions::einsum(temp("i1,i2;a1,a2"), lhs("i1,i2;a1,a3"),
+                            rhs("i1,i2;a2,a3"));
+    man_result("i1,i2;a1,a2") += temp("i1,i2;a1,a2");
+
+    // third term evaluated manually
+    // using rhs from above as the T2 amplitude
+    DA_tot temp2;
+    TA::expressions::einsum(temp2("i1,i2,i3;a1,a2"),
+                            std::get<DA>(tensor_yield(L"f{o;o}"))("i3,i1"),
+                            rhs("i2,i3;a1,a2"));
+    DA_tot temp3;
+    TA::expressions::einsum(
+        temp3("i1,i2,i3;a1,a2"), temp2("i1,i2,i3;a1,a2"),
+        std::get<DA_tot>(tensor_yield(L"O{v<o,o>;v<o,o>}"))("i1,i2,i3;a1"));
+    TA::expressions::einsum(
+        temp("i1,i2;a1,a2"), temp3("i1,i2,i3;a1,a2"),
+        std::get<DA_tot>(tensor_yield(L"O{v<o,o>;v<o,o>}"))("i1,i2,i3;a2"));
+    man_result("i1,i2;a1,a2") -= temp("i1,i2;a1,a2");
+
+    verify_equal_tot_tile(eval_result.find(0).get(), man_result.find(0).get());
+
+    auto eval_result_symm_tot = eval_symm_tot(
+        node, container::vector<std::string>{"i_1", "i_2"},
+        container::vector<std::string>{"a_1", "a_2"}, tensor_yield, cman);
+
+    auto man_result_symm_tot = eval_result;
+    man_result_symm_tot("0,1;2,3") -=
+        eval_result("0,1;2,3");  // zero initial result
+    man_result_symm_tot("0,1;2,3") += eval_result("0,1;2,3");
+    man_result_symm_tot("0,1;2,3") += eval_result("1,0;3,2");
+
+    verify_equal_tot_tile(eval_result_symm_tot.find(0).get(),
+                          man_result_symm_tot.find(0).get());
+
+    auto eval_result_antisymm_tot = eval_antisymm_tot(
+        node, container::vector<std::string>{"i_1", "i_2"},
+        container::vector<std::string>{"a_1", "a_2"}, tensor_yield, cman);
+
+    auto man_result_antisymm_tot = eval_result;
+    man_result_antisymm_tot("0,1;2,3") -=
+        eval_result("0,1;2,3");  // zero initial result
+    man_result_antisymm_tot("0,1;2,3") += eval_result("0,1;2,3");
+
+    man_result_antisymm_tot("0,1;2,3") -= eval_result("1,0;2,3");
+    man_result_antisymm_tot("0,1;2,3") += eval_result("1,0;3,2");
+    man_result_antisymm_tot("0,1;2,3") -= eval_result("0,1;3,2");
+
+    verify_equal_tot_tile(eval_result_antisymm_tot.find(0).get(),
+                          man_result_antisymm_tot.find(0).get());
   }
 }
