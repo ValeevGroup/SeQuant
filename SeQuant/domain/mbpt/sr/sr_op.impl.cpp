a75a9a91cd120554c3b86c5d1648f57c307d320d
Eduard Valeyev <eduard@valeyev.net>
2022-12-19 08:19:22 -0500

    Lambda, Lambda_, L, R, and R12 take antisymm as an arg

--- SeQuant/domain/mbpt/sr/sr_op.impl.cpp
+++ SeQuant/domain/mbpt/sr/sr_op.impl.cpp
@@ -1,98 +1,123 @@
 /// makes excitation operator of bra/ket ranks @c Nbra/Nket
-ExprPtr T_(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds, bool antisymm) {
+ExprPtr T_(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+           bool antisymm) {
   assert(Nbra > 0 && Nbra < std::numeric_limits<std::size_t>::max());
   assert(Nket > 0);
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   return Op(OpType::t, Nbra, Nket_)(complete_unoccupieds, antisymm);
 }
 
 /// makes lambda deexcitation operator of bra/ket ranks @c Nbra/Nket
-ExprPtr Lambda_(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds) {
+ExprPtr Lambda_(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+                bool antisymm) {
   assert(Nbra > 0 && Nbra < std::numeric_limits<std::size_t>::max());
   assert(Nket > 0);
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
-  return Op(OpType::l, Nbra, Nket_)(complete_unoccupieds);
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  return Op(OpType::l, Nbra, Nket_)(complete_unoccupieds, antisymm);
 }
 
 namespace detail {
 
 /// constructs a sum of ops up to a given bra/ket rank
 class op_impl {
-
   OpType op_;
   std::size_t nbra_, nket_;
 
  public:
-  op_impl(OpType op, std::size_t nbra, std::size_t nket = std::numeric_limits<std::size_t>::max()) :
-  op_(op), nbra_(nbra), nket_(nket == std::numeric_limits<std::size_t>::max() ? nbra : nket) {
+  op_impl(OpType op, std::size_t nbra,
+          std::size_t nket = std::numeric_limits<std::size_t>::max())
+      : op_(op),
+        nbra_(nbra),
+        nket_(nket == std::numeric_limits<std::size_t>::max() ? nbra : nket) {
     assert(nbra_ > 0 && nbra_ < std::numeric_limits<std::size_t>::max());
     assert(nket_ > 0);
   }
 
-  void operator()(ExprPtr& result, bool complete_unoccupieds = false, bool antisymm = true) {
+  void operator()(ExprPtr& result, bool complete_unoccupieds = false,
+                  bool antisymm = true) {
     if (op_ == OpType::t)
-      result = result ? result + T_(nbra_, nket_, complete_unoccupieds, antisymm) : T_(nbra_, nket_, complete_unoccupieds, antisymm);
+      result = result
+                   ? result + T_(nbra_, nket_, complete_unoccupieds, antisymm)
+                   : T_(nbra_, nket_, complete_unoccupieds, antisymm);
     else if (op_ == OpType::l)
-      result = result ? result + Lambda_(nbra_, nket_, complete_unoccupieds) : Lambda_(nbra_, nket_, complete_unoccupieds);
+      result = result ? result + Lambda_(nbra_, nket_, complete_unoccupieds,
+                                         antisymm)
+                      : Lambda_(nbra_, nket_, complete_unoccupieds, antisymm);
     else
       assert(false && "unsupported op value");
 
     if ((nbra_ > 1 && nket_ > 0) || (nbra_ > 0 && nket_ > 1)) {
-      op_impl{op_, nbra_ - 1, nket_ - 1}(result, complete_unoccupieds);
+      op_impl{op_, nbra_ - 1, nket_ - 1}(result, complete_unoccupieds,
+                                         antisymm);
     }
   }
-
 };
 
 }  // namespace detail
 
-/// makes excitation operator of all bra/ket ranks up to (and including) @c Nbra/Nket
-ExprPtr T(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds, bool antisymm) {
+/// makes excitation operator of all bra/ket ranks up to (and including) @c
+/// Nbra/Nket
+ExprPtr T(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+          bool antisymm) {
   assert(Nbra > 0 && Nbra < std::numeric_limits<std::size_t>::max());
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   assert(Nket_ > 0);
   ExprPtr result;
-  detail::op_impl{OpType::t, Nbra, Nket_}(result, complete_unoccupieds, antisymm);
+  detail::op_impl{OpType::t, Nbra, Nket_}(result, complete_unoccupieds,
+                                          antisymm);
   return result;
 }
 
-/// makes deexcitation operator of all bra/ket ranks up to (and including) @c Nbra/Nket
-ExprPtr Lambda(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds) {
+/// makes deexcitation operator of all bra/ket ranks up to (and including) @c
+/// Nbra/Nket
+ExprPtr Lambda(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+               bool antisymm) {
   assert(Nbra > 0 && Nbra < std::numeric_limits<std::size_t>::max());
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   assert(Nket_ > 0);
   ExprPtr result;
-  detail::op_impl{OpType::l, Nbra, Nket_}(result, complete_unoccupieds);
+  detail::op_impl{OpType::l, Nbra, Nket_}(result, complete_unoccupieds,
+                                          antisymm);
   return result;
 }
 
 /// makes geminal excitation operator for ansatz @p ansatz
-ExprPtr R12(IndexSpace::Type gg_space, int ansatz) {
+ExprPtr R12(IndexSpace::Type gg_space, int ansatz, bool antisymm) {
   assert(ansatz == 1 || ansatz == 2);
-  bool antisymm = (get_default_context().spbasis() == SPBasis::spinorbital);
-  return Op(OpType::R12, 2, 2)(ansatz == 2 ? IndexSpace::complete_unoccupied : IndexSpace::other_unoccupied, gg_space, antisymm);
+  return Op(OpType::R12, 2, 2)(ansatz == 2 ? IndexSpace::complete_unoccupied
+                                           : IndexSpace::other_unoccupied,
+                               gg_space, antisymm);
 }
 
 /// makes deexcitation operator of bra/ket ranks @c Nbra/Nket
 ExprPtr A(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds) {
   assert(Nbra > 0 && Nbra < std::numeric_limits<std::size_t>::max());
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   assert(Nket_ > 0);
   return Op(OpType::A, Nbra, Nket_)(complete_unoccupieds);
 }
 
 /// makes L deexcitation operator of bra/ket ranks @c Nbra/Nket
-ExprPtr L(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds) {
+ExprPtr L(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+          bool antisymm) {
   assert(Nbra < std::numeric_limits<std::size_t>::max());
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   assert(Nbra > 0 || Nket_ > 0);
-  return Op(OpType::L, Nbra, Nket_)(complete_unoccupieds);
+  return Op(OpType::L, Nbra, Nket_)(complete_unoccupieds, antisymm);
 }
 
 /// makes R excitation operator of bra/ket ranks @c Nbra/Nket
-ExprPtr R(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds) {
+ExprPtr R(std::size_t Nbra, std::size_t Nket, bool complete_unoccupieds,
+          bool antisymm) {
   assert(Nbra < std::numeric_limits<std::size_t>::max());
-  const auto Nket_ = Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
+  const auto Nket_ =
+      Nket == std::numeric_limits<std::size_t>::max() ? Nbra : Nket;
   assert(Nbra > 0 || Nket_ > 0);
-  return Op(OpType::R, Nbra, Nket_)(complete_unoccupieds);
+  return Op(OpType::R, Nbra, Nket_)(complete_unoccupieds, antisymm);
 }
