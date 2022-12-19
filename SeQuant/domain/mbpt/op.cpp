6f89f9e699a3ebc85920572b9c2440d8ec6f69c4
Eduard Valeyev <eduard@valeyev.net>
2022-12-19 08:26:55 -0500

    V is nonhermitian, added Ṽ as its adjoint

--- SeQuant/domain/mbpt/op.cpp
+++ SeQuant/domain/mbpt/op.cpp
@@ -10,7 +10,7 @@ namespace mbpt {
 std::vector<std::wstring>
     cardinal_tensor_labels() {
   return {L"\\lambda",L"\\gamma",L"\\Gamma", L"A", L"S", L"P", L"L", L"λ", L"h", L"f", L"g",
-          L"t", L"R", L"F",L"X", L"V", L"B", L"U",L"GR",L"C", overlap_label(), L"a", L"ã", L"b", L"ᵬ", L"E"};
+          L"t", L"R", L"F",L"X", L"V", L"Ṽ", L"B", L"U",L"GR",L"C", overlap_label(), L"a", L"ã", L"b", L"ᵬ", L"E"};
 }
 
 std::wstring to_wstring(OpType op) {
@@ -36,7 +36,7 @@ std::wstring to_wstring(OpType op) {
     case OpType::GR:
       return L"GR";
     case OpType::C:
-        return L"C";
+      return L"C";
     default:
       throw std::invalid_argument("to_wstring(OpType op): invalid op");
   }
@@ -54,7 +54,8 @@ OpClass to_class(OpType op) {
       return OpClass::ex;
     case OpType::l:
     case OpType::A:
-    case OpType::L: return OpClass::deex;
+    case OpType::L:
+      return OpClass::deex;
     default:
       throw std::invalid_argument("to_class(OpType op): invalid op");
   }
