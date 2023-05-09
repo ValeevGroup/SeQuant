74018cc4b3d9a50226579372ed14c9a65731eb25
Eduard Valeyev <eduard@valeyev.net>
2023-05-09 08:28:02 -0400

    introduced mbpt::Operator that can be used to screen MBPT operator products

--- SeQuant/domain/mbpt/op.hpp
+++ SeQuant/domain/mbpt/op.hpp
@@ -8,22 +8,25 @@
 #include <string>
 #include <vector>
 
+#include <SeQuant/core/attr.hpp>
+#include <SeQuant/core/expr.hpp>
+
 namespace sequant {
 namespace mbpt {
 
-/// Operators
+/// enumerates the known Operator types
 enum class OpType {
-  h,   //!< 1-body Hamiltonian
-  f,   //!< Fock operator
-  g,   //!< 2-body Coulomb
-  t,   //!< cluster amplitudes
-  l,   //!< deexcitation cluster amplitudes
-  A,   //!< antisymmetrizer
-  L,   //!< left-hand eigenstate
-  R,   //!< right-hand eigenstate
-  R12, //!< geminal kernel
-  GR,  //!< GR kernel from f12 theory
-  C    //!< cabs singles op
+  h,    //!< 1-body Hamiltonian
+  f,    //!< Fock operator
+  g,    //!< 2-body Coulomb
+  t,    //!< cluster amplitudes
+  l,    //!< deexcitation cluster amplitudes
+  A,    //!< antisymmetrizer
+  L,    //!< left-hand eigenstate
+  R,    //!< right-hand eigenstate
+  R12,  //!< geminal kernel
+  GR,   //!< GR kernel from f12 theory
+  C     //!< cabs singles op
 };
 
 /// Operator character relative to Fermi vacuum
@@ -34,6 +37,85 @@ std::vector<std::wstring> cardinal_tensor_labels();
 
 std::wstring to_wstring(OpType op);
 OpClass to_class(OpType op);
+
+//////////////////////////////
+
+/// mbpt::Operator = Tensor * NormalOperator
+
+/// It is an abstract (index-free) representation of a many-body operator
+/// that tracks one or more quantum numbers (e.g., excitation level for
+/// single-reference MBPT, particle numbers in each subspace for multi-reference
+/// MBPT, etc.) and can be used for operator-level optimizations.
+template <typename QuantumNumbers, Statistics S = Statistics::FermiDirac>
+class Operator;
+
+template <typename QuantumNumbers>
+using FOperator = Operator<QuantumNumbers, Statistics::FermiDirac>;
+template <typename QuantumNumbers>
+using BOperator = Operator<QuantumNumbers, Statistics::BoseEinstein>;
+
+/// Operator<void> does not track any quantum numbers, this is a base for
+/// Operator
+template <Statistics S>
+class Operator<void, S> : public Expr {
+ protected:
+  Operator() = default;
+  Operator(std::function<std::wstring()> label_generator,
+           std::function<ExprPtr()> tensor_form_generator)
+      : label_generator_(std::move(label_generator)),
+        tensor_form_generator_(tensor_form_generator) {}
+
+ public:
+  virtual ~Operator() = default;
+
+  virtual std::wstring label() const {
+    assert(label_generator_);
+    return label_generator_();
+  }
+
+  virtual ExprPtr tensor_form() const {
+    assert(tensor_form_generator_);
+    return tensor_form_generator_();
+  }
+
+ private:
+  std::function<std::wstring()> label_generator_;
+  std::function<ExprPtr()> tensor_form_generator_;
+};
+
+using FOperatorBase = FOperator<void>;
+using BOperatorBase = BOperator<void>;
+
+template <typename QuantumNumbers, Statistics S>
+class Operator : public Operator<void> {
+  using base_type = Operator<void>;
+
+ protected:
+  Operator() = default;
+
+ public:
+  Operator(std::function<std::wstring()> label_generator,
+           std::function<ExprPtr()> tensor_form_generator,
+           std::function<void(QuantumNumbers&)> qn_action)
+      : base_type(std::move(label_generator), std::move(tensor_form_generator)),
+        qn_action_(std::move(qn_action)) {}
+  virtual ~Operator() = default;
+
+  QuantumNumbers operator()(const QuantumNumbers& qns) const {
+    QuantumNumbers result(qns);
+    this->apply_to(result);
+    return result;
+  }
+
+  virtual QuantumNumbers& apply_to(QuantumNumbers& qns) const {
+    assert(qn_action_);
+    qn_action_(qns);
+    return qns;
+  }
+
+ private:
+  std::function<void(QuantumNumbers&)> qn_action_;
+};  // Operator
 
 }  // namespace mbpt
 }  // namespace sequant
--- tests/unit/test_mbpt.cpp
+++ tests/unit/test_mbpt.cpp
@@ -2,11 +2,60 @@
 // Created by Eduard Valeyev on 2019-02-19.
 //
 
+#include "SeQuant/core/op.hpp"
 #include "SeQuant/core/tensor.hpp"
 #include "SeQuant/core/timer.hpp"
 #include "SeQuant/domain/mbpt/formalism.hpp"
+#include "SeQuant/domain/mbpt/op.hpp"
 #include "SeQuant/domain/mbpt/sr/sr.hpp"
+#include "SeQuant/external/boost/interval.hpp"
+
 #include "catch.hpp"
+
+TEST_CASE("NBodyOp", "[mbpt]") {
+  using namespace sequant;
+
+  SECTION("constructor") {
+    using qns_t = boost::numeric::interval<int>;
+    using op_t = mbpt::Operator<qns_t>;
+    op_t f1([]() { return L"F"; },
+            []() -> ExprPtr {
+              return ex<Tensor>(L"F", WstrList{L"p_1"}, WstrList{L"p_2"}) *
+                     ex<FNOperator>(WstrList{L"p_1"}, WstrList{L"p_2"});
+            },
+            [](qns_t& qns) {
+              qns += qns_t{-1, 1};
+            });
+    REQUIRE(f1.label() == L"F");
+    {  // possible compare
+      using namespace boost::numeric::interval_lib::compare::possible;
+      // REQUIRE(f1(qns_t{0, 0}) == 1);   // this is not same as below, due to
+      // Catch interference
+      REQUIRE(operator==(f1(qns_t{0, 0}), 1));  // can produce single excitation
+      REQUIRE(operator==(f1(qns_t{0, 0}),
+                         -1));  // can produce single de-excitation
+      REQUIRE(operator==(
+          f1(qns_t{0, 0}),
+          0));  // F is normal, so must excite/deexcite, but there is no way to
+                // exclude 0 from boost::interbal    }
+      REQUIRE(operator==(
+          f1(qns_t{1, 1}),
+          0));  // can produce reference when acting on singly-excited
+      REQUIRE(operator!=(
+          f1(qns_t{2, 2}),
+          0));  // cannot produce reference when acting on doubly-excited
+    }
+    {  // equal compare
+      // using namespace boost::numeric::interval_lib::compare::lexicographic;
+      // REQUIRE(f1(qns_t{0, 0}) == qns_t{-1, 1}); // not same as below due to
+      // interaction with Catch
+      // could do REQUIRE(operator==(f1(qns_t{0, 0}), qns_t{-1, 1})); but equal
+      // is shorter
+      REQUIRE(equal(f1(qns_t{0, 0}), qns_t{-1, 1}));
+      REQUIRE(equal(f1(qns_t{-1, 1}), qns_t{-2, 2}));
+    }
+  }
+}
 
 TEST_CASE("MBPT", "[mbpt]") {
   using namespace sequant;
