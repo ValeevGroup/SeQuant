4d8c361f9342cbb0cf6726da6deece00ec3eb3cb
Eduard Valeyev <eduard@valeyev.net>
2023-05-12 06:17:09 -0400

    import MPQC's set_locale

--- SeQuant/core/runtime.hpp
+++ SeQuant/core/runtime.hpp
@@ -5,11 +5,11 @@
 #ifndef SEQUANT_RUNTIME_HPP
 #define SEQUANT_RUNTIME_HPP
 
+#include <atomic>
 #include <mutex>
 #include <stdexcept>
 #include <thread>
 #include <vector>
-#include <atomic>
 
 namespace sequant {
 
@@ -30,16 +30,17 @@ inline void set_num_threads(int nt) {
 }
 
 /// @return the number of threads to use for concurrent work
-/// @note by default use the value returned std::thread::hardware_concurrency() if available, otherwise 1
+/// @note by default use the value returned std::thread::hardware_concurrency()
+/// if available, otherwise 1
 /// @sa set_num_threads()
-inline int num_threads() {
-  return detail::nthreads_accessor();
-}
+inline int num_threads() { return detail::nthreads_accessor(); }
 
-/// Fires off @c nthreads instances of lambda in parallel, each in its own thread (thus @c nthreads-1 std::thread objects are created),
-/// where @c nthreads is the value returned by get_num_threads() .
+/// Fires off @c nthreads instances of lambda in parallel, each in its own
+/// thread (thus @c nthreads-1 std::thread objects are created), where @c
+/// nthreads is the value returned by get_num_threads() .
 /// @tparam Lambda a function type for which @c Lambda(int) is valid
-/// @param lambda the function object to execute, each will be invoked as @c lambda(thread_id) where @c thread_id is an integer in
+/// @param lambda the function object to execute, each will be invoked as @c
+/// lambda(thread_id) where @c thread_id is an integer in
 ///        @c [0,nthreads) .
 /// @sa get_num_threads()
 template <typename Lambda>
@@ -56,11 +57,14 @@ void parallel_do(Lambda&& lambda) {
     threads[thread_id].join();
 }
 
-/// Fires off @c ntasks instances of lambda in parallel, with at most @c nthreads instances executing concurrently,
-/// where @c nthreads is the value returned by get_num_threads() .
+/// Fires off @c ntasks instances of lambda in parallel, with at most @c
+/// nthreads instances executing concurrently, where @c nthreads is the value
+/// returned by get_num_threads() .
 /// @tparam Lambda a function type for which @c Lambda(int) is valid
-/// @param lambda the function object to execute, each will be invoked as @c lambda(task_id) where @c task_id is an integer in
-///        @c [0,ntasks) . @c lambda(t1) will be commenced not after @c lambda(t2) if @c t1<t2 .
+/// @param lambda the function object to execute, each will be invoked as @c
+/// lambda(task_id) where @c task_id is an integer in
+///        @c [0,ntasks) . @c lambda(t1) will be commenced not after @c
+///        lambda(t2) if @c t1<t2 .
 /// @node The load is balanced dynamically.
 /// @sa get_num_threads()
 template <typename Lambda>
@@ -68,7 +72,7 @@ void parallel_for_each(Lambda&& lambda, const size_t ntasks) {
   std::atomic<size_t> work = 0;
   auto task = [&work, &lambda, ntasks](int thread_id) {
     size_t task_id = work.fetch_add(1);
-    while(task_id < ntasks) {
+    while (task_id < ntasks) {
       std::forward<Lambda>(lambda)(task_id);
       task_id = work.fetch_add(1);
     }
@@ -86,7 +90,8 @@ void parallel_for_each(Lambda&& lambda, const size_t ntasks) {
     threads[thread_id].join();
 }
 
-}
+void set_locale();
 
+}  // namespace sequant
 
-#endif //SEQUANT_RUNTIME_HPP
+#endif  // SEQUANT_RUNTIME_HPP
--- tests/unit/test_main.cpp
+++ tests/unit/test_main.cpp
@@ -23,15 +23,9 @@ int main(int argc, char* argv[]) {
   Catch::Session session;
 
   // global setup...
-  std::locale::global(std::locale("en_US.UTF-8"));
   std::wcout.precision(std::numeric_limits<double>::max_digits10);
   std::wcerr.precision(std::numeric_limits<double>::max_digits10);
-  std::wcout.sync_with_stdio(false);
-  std::wcerr.sync_with_stdio(false);
-  std::wcout.imbue(std::locale("en_US.UTF-8"));
-  std::wcerr.imbue(std::locale("en_US.UTF-8"));
-  std::wcout.sync_with_stdio(true);
-  std::wcerr.sync_with_stdio(true);
+  sequant::set_locale();
   detail::OpIdRegistrar op_id_registrar;
   sequant::set_default_context(
       SeQuant(Vacuum::SingleProduct, IndexSpaceMetric::Unit,
