7de8ededf7f69ee007284d16935bb8bc8049ca5a
Eduard Valeyev <eduard@valeyev.net>
2023-05-18 12:52:54 -0400

    prepend size_t in math.hpp with std:: to pacify older gcc

--- SeQuant/core/math.hpp
+++ SeQuant/core/math.hpp
@@ -6,38 +6,38 @@
 #define SEQUANT_CORE_MATH_HPP
 
 #include <assert.h>
-#include <cstdint>
+#include <cstddef>
 
 namespace sequant {
 
-constexpr auto pow2(size_t n) {
+constexpr std::size_t pow2(std::size_t n) {
   assert(n <= 63);
   return 1ul << n;
 }
 
-constexpr inline int64_t factorial(int64_t n) {
+constexpr inline std::size_t factorial(std::size_t n) {
   assert(n >= 0 && n <= 20);
-  const int64_t values[] = {1,
-                            1,
-                            2,
-                            6,
-                            24,
-                            120,
-                            720,
-                            5040,
-                            40320,
-                            362880,
-                            3628800,
-                            39916800,
-                            479001600,
-                            6227020800,
-                            87178291200,
-                            1307674368000,
-                            20922789888000,
-                            355687428096000,
-                            6402373705728000,
-                            121645100408832000,
-                            2432902008176640000};
+  const std::size_t values[] = {1,
+                                1,
+                                2,
+                                6,
+                                24,
+                                120,
+                                720,
+                                5040,
+                                40320,
+                                362880,
+                                3628800,
+                                39916800,
+                                479001600,
+                                6227020800,
+                                87178291200,
+                                1307674368000,
+                                20922789888000,
+                                355687428096000,
+                                6402373705728000,
+                                121645100408832000,
+                                2432902008176640000};
   return values[n];
 }
 
