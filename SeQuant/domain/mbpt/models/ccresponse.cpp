cda0ed7833bcf5ee7dd82913a725f7932c03e4b4
Ajay <ajaymk22@vt.edu>
2023-07-12 21:51:24 -0400

    [WIP] Basic layout for implementing coupled-cluster response

---
+++ SeQuant/domain/mbpt/models/ccresponse.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Ajay Melekamburath on 7/12/23.
+//
+
+#include <SeQuant/domain/mbpt/models/ccresponse.hpp>
+
+namespace sequant::mbpt::sr {
+
+// here, add the definitions for the functions declared in ccresponse.hpp
+
+}  // namespace sequant::mbpt::sr
---
+++ SeQuant/domain/mbpt/models/ccresponse.hpp
@@ -0,0 +1,35 @@
+//
+// Created by Ajay Melekamburath on 7/12/23.
+//
+
+#ifndef SEQUANT_CCRESPONSE_HPP
+#define SEQUANT_CCRESPONSE_HPP
+
+#include <SeQuant/domain/mbpt/models/cc.hpp>
+
+// clang-format off
+// 1. implement the one-body operator to represent perturbations
+// 2. implement the tensor/operator to represent perturbed operators
+// 3. add a ccresponse class to derive the residual equations
+// 4. construct perturbed Hamiltonian (try first order for now)
+// 5. solve perturbed t amplitudes (try first order for now)
+// 6. solve perturbed lambda amplitudes (try first order for now)
+// clang-format on
+
+namespace sequant::mbpt::sr {
+
+/// derives residual equations for coupled-cluster response theory
+/// @param N coupled-cluster rank
+/// @param R order of perturbation
+
+class ccresponse {
+  size_t N, R;
+
+ public:
+  // functions for deriving derive perturbed t and lambda amplitudes
+  std::vector<sequant::ExprPtr> t();
+  std::vector<sequant::ExprPtr> lambda();
+};
+}  // namespace sequant::mbpt::sr
+
+#endif  // SEQUANT_CCRESPONSE_HPP
