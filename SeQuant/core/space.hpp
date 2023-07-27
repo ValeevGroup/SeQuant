ee753f9f3e407e04e8ed2f6d2c5d6b29a4a502d2
Robert Adam <dev@robert-adam.de>
2023-07-27 13:19:21 +0200

    Improve missing space instance error msg
    Instead of only throwing a bad_attr exception without any (useful) error
    message, we now construct a string representation of the requested space
    type and quantum numbers.
    This should make debugging missing-instance errors more easily.
    
    Step towards #97

--- SeQuant/core/space.hpp
+++ SeQuant/core/space.hpp
@@ -7,6 +7,8 @@
 
 #include <bitset>
 #include <cassert>
+#include <codecvt>
+#include <locale>
 
 #include "attr.hpp"
 #include "container.hpp"
@@ -107,6 +109,9 @@ struct QuantumNumbersAttr {
     return QuantumNumbersAttr(-0);
   }
 };
+
+std::wstring to_wstring(TypeAttr type);
+std::wstring to_wstring(QuantumNumbersAttr qns);
 
 /// @brief space of Index objects
 ///
@@ -316,6 +321,7 @@ class IndexSpace {
   };
   struct bad_attr : std::invalid_argument {
     bad_attr() : std::invalid_argument("bad attribute") {}
+    using std::invalid_argument::invalid_argument;
   };
 
   struct KeyCompare {
@@ -354,7 +360,12 @@ class IndexSpace {
     const auto attr = Attr(type, qns);
     assert(attr.is_valid());
     if (attr == Attr::null()) return null_instance();
-    if (!instance_exists(attr)) throw bad_attr();
+    if (!instance_exists(attr)) {
+      std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+      throw bad_attr(converter.to_bytes(L"Request to non-existing space: " +
+                                        sequant::to_wstring(type) + L" " +
+                                        sequant::to_wstring(qns)));
+    }
     return instances_.find(attr)->second;
   }
 
