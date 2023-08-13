eb741af5124be41725f51d035b7d93fb5547c75f
Bimal Gaudel <bimalgaudel@gmail.com>
2023-08-12 23:55:44 -0400

    Log TiledArray memory use if sequant eval log level is 3 or more.

--- SeQuant/domain/eval/eval_result.hpp
+++ SeQuant/domain/eval/eval_result.hpp
@@ -473,6 +473,57 @@ inline void log_constant(Args const&... args) noexcept {
 #endif
 }
 
+/// TA::Tensor memory use logger
+/// If TiledArray was configured with TA_TENSOR_MEM_PROFILE set this
+/// prints the current use of memory by TA::Tensor objects in host memory space
+/// to \p os .
+/// \param world the world object to use for logging
+/// \param label string to prepend to the profile
+void log_ta_tensor_host_memory_use(madness::World& world,
+                                   std::string_view label = "") {
+#if defined(TA_TENSOR_MEM_PROFILE) && defined(SEQUANT_EVAL_TRACE)
+  auto& logger = Logger::get_instance();
+  if (logger.log_level_eval < 3) return;
+  std::vector<std::uint64_t> hwsize(world.size(), 0);
+  std::vector<std::uint64_t> currsize(world.size(), 0);
+  std::vector<std::uint64_t> actsize(world.size(), 0);
+  hwsize[world.rank()] =
+      TA::hostEnv::instance()->host_allocator_getActualHighWatermark();
+  currsize[world.rank()] =
+      TA::hostEnv::instance()->host_allocator().getCurrentSize();
+  actsize[world.rank()] =
+      TA::hostEnv::instance()->host_allocator().getActualSize();
+  world.gop.sum(hwsize.data(), hwsize.size());
+  world.gop.sum(currsize.data(), currsize.size());
+  world.gop.sum(actsize.data(), actsize.size());
+
+  std::ostringstream oss;
+  oss << label << ": TA_TENSOR_MEM_PROFILE allocation statistics (MiB):\n";
+  oss << std::setw(5) << "rank"  //
+      << std::setw(12) << "hw"   //
+      << std::setw(12) << "cur"  //
+      << std::setw(12) << "act"  //
+      << '\n';                   //
+  oss << "--------------------------------------------\n";
+  std::uint64_t total = 0;
+  for (auto rank = 0; rank != world.size(); ++rank) {
+    oss << std::setw(5) << rank                         //
+        << std::setw(12) << hwsize[rank] / (1 << 20)    //
+        << std::setw(12) << currsize[rank] / (1 << 20)  //
+        << std::setw(12) << actsize[rank] / (1 << 20)   //
+        << '\n';
+    total += currsize[rank] / (1 << 20);
+  }
+  oss << std::setw(5) << "total"  //
+      << std::setw(12) << ""      //
+      << std::setw(12) << total   //
+      << std::setw(12) << ""      //
+      << '\n';
+  oss << "--------------------------------------------" << std::endl;
+  write_log(logger, oss.str());
+#endif
+}
+
 }  // namespace
 
 struct EvalResult;
@@ -725,6 +776,7 @@ class EvalTensorTA final : public EvalResult {
     T result;
     result(a.this_annot) = get<T>()(a.lannot) + other.get<T>()(a.rannot);
     decltype(result)::wait_for_lazy_cleanup(result.world());
+    log_ta_tensor_host_memory_use(TA::get_default_world(), "[TA] ");
     return eval_result<EvalTensorTA<T>>(std::move(result));
   }
 
@@ -742,6 +794,7 @@ class EvalTensorTA final : public EvalResult {
       result(a.this_annot) = scalar * result(a.lannot);
 
       decltype(result)::wait_for_lazy_cleanup(result.world());
+      log_ta_tensor_host_memory_use(TA::get_default_world(), "[TA] ");
       return eval_result<EvalTensorTA<T>>(std::move(result));
     }
 
@@ -776,6 +829,7 @@ class EvalTensorTA final : public EvalResult {
     T result;
     result(post_annot) = get<T>()(pre_annot);
     T::wait_for_lazy_cleanup(result.world());
+    log_ta_tensor_host_memory_use(TA::get_default_world(), "[TA] ");
     return eval_result<EvalTensorTA<T>>(std::move(result));
   }
 
@@ -790,6 +844,7 @@ class EvalTensorTA final : public EvalResult {
 
     t(ann) += o(ann);
     T::wait_for_lazy_cleanup(t.world());
+    log_ta_tensor_host_memory_use(TA::get_default_world(), "[TA] ");
   }
 
   [[nodiscard]] ERPtr symmetrize(
