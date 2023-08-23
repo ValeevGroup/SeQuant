5f51915c8d549f97856a30f2f6b4e1ee7e812e62
Robert Adam <dev@robert-adam.de>
2023-08-23 13:03:23 +0200

    Revamp and fix enable-ifs in expr.hpp
    The used enable-ifs looked a bit scary and have therefore been
    refactored to use intermediates with (hopefully) clear names.
    
    Furthermore, a couple of tests in those enable-ifs have been performed
    with std::is_same, but only removing const from the compared type and
    thus these checks would not work properly as soon as a reference type
    was passed.
    This could be observed while constructing a Product from a non-const
    other product. Thus, the type of the to-be-copied object was just
    Product & and not const Product &, which is what the copy-ctor
    (correctly) is defined for. However, there is the templated
    Product(Range &&) ctor, that thanks to the universal reference is the
    more specialized candidate in this case (it takes the type as-is without
    requiring a const to be slapped on). This would lead to the product
    being copied, but also flattened (as this is the default behavior of the
    range-ctor).

--- SeQuant/core/meta.hpp
+++ SeQuant/core/meta.hpp
@@ -226,6 +226,26 @@ static constexpr bool is_range_v =
     (is_detected_v<is_range_impl::ranges_begin_t, T> &&
      is_detected_v<is_range_impl::ranges_end_t, T>);
 
+
+/// is_same
+/// Checks whether \c T is a \c Base (is either the same class or a sub-class
+/// ignoring CV and reference qualifiers
+template <typename Base, typename T>
+using is_base_of = std::is_base_of<remove_cvref_t<Base>, remove_cvref_t<T>>;
+template <typename Base, typename T>
+constexpr bool is_base_of_v = is_base_of<Base, T>::value;
+
+/// is_same
+/// Checks whether \c T and \c U are the same type, ignoring any CV and
+/// reference qualifiers
+template <typename T, typename U>
+struct is_same
+    : std::bool_constant<std::is_same_v<remove_cvref_t<T>, remove_cvref_t<U>>> {
+};
+
+template <typename T, typename U>
+constexpr bool is_same_v = is_same<T, U>::value;
+
 }  // namespace meta
 }  // namespace sequant
 
