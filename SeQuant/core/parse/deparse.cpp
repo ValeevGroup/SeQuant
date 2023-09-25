12a0eb95c2041dc0100db1f996717d486deed450
Robert Adam <dev@robert-adam.de>
2023-09-25 10:52:22 +0200

    Extended deparse_expr & remove regex from it

--- SeQuant/core/parse/deparse.cpp
+++ SeQuant/core/parse/deparse.cpp
@@ -1,110 +1,145 @@
 #include "SeQuant/core/parse_expr.hpp"
 
+#include <SeQuant/core/attr.hpp>
 #include <SeQuant/core/expr.hpp>
 #include <SeQuant/core/index.hpp>
-#include <SeQuant/core/parse/regex_sequant.hpp>
 #include <SeQuant/core/tensor.hpp>
 
-#include <boost/regex.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+#include <cassert>
+#include <codecvt>
+#include <locale>
+#include <string>
 
 namespace sequant {
 
 namespace details {
-std::wstring deparse_expr(Tensor const& tnsr, bool annot_sym) {
-  std::wstring idx_str = tnsr.label().data();
 
-  auto add_idx = [&idx_str](Index const& idx) {
-    std::wstring str = idx.label().data();
-    if (idx.has_proto_indices()) {
-      str += L"<";  // begin proto-indices token
-      for (auto&& pi : idx.proto_indices()) {
-        str += pi.label().data();
-        str += L",";
+std::wstring deparse_index(const Index& index) {
+  std::wstring deparsed(index.label());
+
+  if (index.has_proto_indices()) {
+    deparsed += L"<";
+    const auto& protos = index.proto_indices();
+    for (std::size_t i = 0; i < protos.size(); ++i) {
+      deparsed += protos[i].label();
+
+      if (i + 1 < protos.size()) {
+        deparsed += L", ";
       }
-      // remove that trailing comma
-      str.pop_back();
-      str += L">";  // end proto-indices token
     }
-    idx_str += str;
-  };
-
-  idx_str += L"{";  // open tensor bra-ket and begin bra
-  for (auto const& idx : tnsr.bra()) {
-    add_idx(idx);
-    idx_str += L",";
-  }
-  *idx_str.rbegin() = L';';  // begin ket
-  for (auto const& idx : tnsr.ket()) {
-    add_idx(idx);
-    idx_str += L",";
+    deparsed += L">";
   }
 
-  *idx_str.rbegin() = L'}';  // close tensor bra-ket
-  if (annot_sym) {
-    if (tnsr.symmetry() == Symmetry::antisymm)
-      idx_str += L":A";
-    else if (tnsr.symmetry() == Symmetry::symm)
-      idx_str += L":S";
-    else if (tnsr.symmetry() == Symmetry::nonsymm)
-      idx_str += L":N";
-    else
-      idx_str += L":INVALID";
+  return deparsed;
+}
+
+template <typename Range>
+std::wstring deparse_indices(const Range& indices) {
+  std::wstring deparsed;
+
+  for (std::size_t i = 0; i < indices.size(); ++i) {
+    deparsed += deparse_index(indices[i]);
+
+    if (i + 1 < indices.size()) {
+      deparsed += L", ";
+    }
   }
-  return idx_str;
+
+  return deparsed;
+}
+
+std::wstring deparse_sym(Symmetry sym) {
+  switch (sym) {
+    case Symmetry::symm:
+      return L"S";
+    case Symmetry::antisymm:
+      return L"A";
+    case Symmetry::nonsymm:
+      return L"N";
+    case Symmetry::invalid:
+      return L"INVALID";
+  }
+
+  assert(false);
+  return L"INVALIDANDUNREACHABLE";
+}
+
+std::wstring deparse_expr(Tensor const& tensor, bool annot_sym) {
+  return std::wstring(tensor.label()) + L"^{" + deparse_indices(tensor.ket()) +
+         L"}_{" + deparse_indices(tensor.bra()) + L"}" +
+         (annot_sym ? std::wstring(L":") + deparse_sym(tensor.symmetry())
+                    : std::wstring{});
+}
+
+std::wstring deparse_scalar(const Constant::scalar_type& scalar) {
+  const auto& real = scalar.real();
+  const auto& realNumerator = boost::multiprecision::numerator(real);
+  const auto& realDenominator = boost::multiprecision::denominator(real);
+  const auto& imag = scalar.imag();
+  const auto& imagNumerator = boost::multiprecision::numerator(imag);
+  const auto& imagDenominator = boost::multiprecision::denominator(imag);
+
+  std::string deparsed;
+  if (realNumerator != 0) {
+    deparsed += realNumerator.str();
+
+    if (realDenominator != 1) {
+      deparsed += "/" + realDenominator.str();
+    }
+  }
+  if (imagNumerator != 0) {
+    if (!deparsed.empty()) {
+      deparsed += imagNumerator < 0 ? " + i " : " - i ";
+    }
+
+    deparsed += imagNumerator.str();
+
+    if (imagDenominator != 1) {
+      deparsed += "/" + imagDenominator.str();
+    }
+  }
+
+  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+  return converter.from_bytes(deparsed);
+}
+
+std::wstring deparse_expr(const Constant& constant) {
+  return deparse_scalar(constant.value());
+}
+
+std::wstring deparse_expr(const Variable& variable) {
+  return std::wstring(variable.label());
 }
 
 std::wstring deparse_expr(Product const& prod, bool annot_sym) {
-  auto str = std::wstring{};
+  std::wstring deparsed;
 
-  auto scal = prod.scalar();
+  const auto& scal = prod.scalar();
   if (scal != Product::scalar_type{1}) {
-    std::wstring scalar_latex = Constant{scal}.to_latex();
-
-    static auto const decimal_rgx =
-        boost::wregex{std::wstring{} + L"(-)?" +
-                      parse::regex_patterns::abs_real_num().data()};
-
-    container::svector<std::wstring, 2> num_denom{};
-    auto const end = boost::wsregex_iterator{};
-    for (auto iter = boost::wsregex_iterator{scalar_latex.begin(),
-                                             scalar_latex.end(), decimal_rgx};
-         iter != end; ++iter) {
-      std::wstring n{};
-      if ((*iter)[1].matched) n += (*iter)[1].str() + L" ";
-      n += (*iter)[2].str();
-      num_denom.emplace_back(n);
-    }
-
-    assert(num_denom.size() == 1 || num_denom.size() == 2);
-
-    std::wstring scalar_text =
-        num_denom[0] + (num_denom.size() == 2 ? L"/" + num_denom[1] : L"");
-
-    if (scalar_text == L"- 1")
-      scalar_text.pop_back();
-    else if (scalar_text == L"1")
-      scalar_text.pop_back();
-
-    if (*scalar_text.rbegin() != L' ' && !scalar_text.empty())
-      scalar_text += L" * ";
-    str += scalar_text;
+    deparsed += deparse_scalar(scal) + L" ";
   }
 
-  str += deparse_expr(prod.factor(0), annot_sym);
+  deparsed += deparse_expr(prod.factor(0), annot_sym);
   for (auto&& xpr : ranges::views::tail(prod.factors()))
-    str += L" * " + deparse_expr(xpr, annot_sym);
+    deparsed += L" * " + deparse_expr(xpr, annot_sym);
 
-  return str;
+  return deparsed;
 }
 
 std::wstring deparse_expr(Sum const& sum, bool annot_sym) {
-  auto str = deparse_expr(sum.summand(0), annot_sym);
+  std::wstring deparsed = deparse_expr(sum.summand(0), annot_sym);
   for (auto&& xpr : ranges::views::tail(sum.summands())) {
-    auto to_app = deparse_expr(xpr, annot_sym);
-    str += (to_app.front() == L'-' ? L" " : L" + ");
-    str += to_app;
+    std::wstring current = deparse_expr(xpr, annot_sym);
+
+    if (current.front() == L'-') {
+      deparsed += L" - " + current.substr(1);
+    } else {
+      deparsed += L" + " + current;
+    }
   }
-  return str;
+  return deparsed;
 }
 
 }  // namespace details
@@ -117,6 +152,10 @@ std::wstring deparse_expr(ExprPtr expr, bool annot_sym) {
     return deparse_expr(expr->as<Sum>(), annot_sym);
   else if (expr->is<Product>())
     return deparse_expr(expr->as<Product>(), annot_sym);
+  else if (expr->is<Constant>())
+    return deparse_expr(expr->as<Constant>());
+  else if (expr->is<Variable>())
+    return deparse_expr(expr->as<Variable>());
   else
     throw std::runtime_error("Unsupported expr type for deparse!");
 }
