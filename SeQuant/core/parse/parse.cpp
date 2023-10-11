a2df01c41ee71ce02ed5347049665ffac6ed9d9a
Robert Adam <dev@robert-adam.de>
2023-10-11 09:06:30 +0200

    Allow selected special chars in index names

--- SeQuant/core/parse/parse.cpp
+++ SeQuant/core/parse/parse.cpp
@@ -84,8 +84,10 @@ auto number_def       = x3::double_ >> -('/' >> x3::double_);
 
 auto variable_def     = x3::lexeme[name >> -(x3::lit('^') >> '*' >> x3::attr(true))];
 
+auto index_name       = +(x3::unicode::alpha | L'⁺' | L'⁻' | L'↑' | L'↓');
+
 auto index_label_def  = x3::lexeme[
-                               +x3::unicode::alpha >> -x3::lit('_') >> x3::uint_
+                               index_name >> -x3::lit('_') >> x3::uint_
                         ];
 
 auto index_def        = x3::lexeme[
