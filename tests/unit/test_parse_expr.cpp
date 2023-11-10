7d2b9e8f07fbac9b15435c325a53813a7f1e9306
Eduard Valeyev <eduard@valeyev.net>
2023-11-10 08:58:53 -0500

    [unit] cleanup test_parse_expr to avoid dereferencing parse_expr result before comparison

--- tests/unit/test_parse_expr.cpp
+++ tests/unit/test_parse_expr.cpp
@@ -71,10 +71,10 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(expr->as<Tensor>().ket().size() == 1);
     REQUIRE(expr->as<Tensor>().ket().at(0) == L"a_1");
 
-    REQUIRE(*expr == *parse_expr(L"t_{i1}^{a1}"));
-    REQUIRE(*expr == *parse_expr(L"t^{a1}_{i1}"));
-    REQUIRE(*expr == *parse_expr(L"t{i_1; a_1}"));
-    REQUIRE(*expr == *parse_expr(L"t_{i_1}^{a_1}"));
+    REQUIRE(expr == parse_expr(L"t_{i1}^{a1}"));
+    REQUIRE(expr == parse_expr(L"t^{a1}_{i1}"));
+    REQUIRE(expr == parse_expr(L"t{i_1; a_1}"));
+    REQUIRE(expr == parse_expr(L"t_{i_1}^{a_1}"));
 
     expr = parse_expr(L"t{i1,i2;a1,a2}");
     REQUIRE(expr->as<Tensor>().bra().size() == 2);
@@ -84,9 +84,9 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(expr->as<Tensor>().ket().at(0).label() == L"a_1");
     REQUIRE(expr->as<Tensor>().ket().at(1).label() == L"a_2");
 
-    REQUIRE(*expr == *parse_expr(L"+t{i1, i2; a1, a2}"));
+    REQUIRE(expr == parse_expr(L"+t{i1, i2; a1, a2}"));
     REQUIRE(parse_expr(L"-t{i1;a1}")->is<Product>());
-    REQUIRE(*expr == *parse_expr(L"t{\ti1, \ti2; \na1,\t a2 \t}"));
+    REQUIRE(expr == parse_expr(L"t{\ti1, \ti2; \na1,\t a2 \t}"));
 
     // Tensor labels including underscores
     REQUIRE(parse_expr(L"T_1{i_1;a_1}")->as<Tensor>().label() == L"T_1");
@@ -157,8 +157,8 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
 
     auto const& prod = expr->as<Product>();
     REQUIRE(prod.scalar() == rational{-1, 2});
-    REQUIRE(*prod.factor(0) == *parse_expr(L"g_{i_2, i_3}^{i_1, a_2}"));
-    REQUIRE(*prod.factor(1) == *parse_expr(L"t^{i2, i3}_{a1, a2}"));
+    REQUIRE(prod.factor(0) == parse_expr(L"g_{i_2, i_3}^{i_1, a_2}"));
+    REQUIRE(prod.factor(1) == parse_expr(L"t^{i2, i3}_{a1, a2}"));
     REQUIRE(parse_expr(L"-1/2 * δ * t{i1;a1}") ==
             parse_expr(L"-1/2  δ  t{i1;a1}"));
     auto const prod2 = parse_expr(L"-1/2 * δ * γ * t{i1;a1}")->as<Product>();
@@ -175,16 +175,16 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(expr1->is<Sum>());
 
     auto const& sum1 = expr1->as<Sum>();
-    REQUIRE(*sum1.summand(0) == *parse_expr(L"f{a1;i1}"));
-    REQUIRE(*sum1.summand(1) ==
-            *parse_expr(L"- 1/2 * g{i2,a1; a2,a3} * t{a2,a3; i1,i2}"));
+    REQUIRE(sum1.summand(0) == parse_expr(L"f{a1;i1}"));
+    REQUIRE(sum1.summand(1) ==
+            parse_expr(L"- 1/2 * g{i2,a1; a2,a3} * t{a2,a3; i1,i2}"));
 
     auto expr2 = parse_expr(L"a - 4");
     REQUIRE(expr2->is<Sum>());
 
     auto const& sum2 = expr2->as<Sum>();
-    REQUIRE(*sum2.summand(0) == *parse_expr(L"a"));
-    REQUIRE(*sum2.summand(1) == *parse_expr(L"-4"));
+    REQUIRE(sum2.summand(0) == parse_expr(L"a"));
+    REQUIRE(sum2.summand(1) == parse_expr(L"-4"));
   }
 
   SECTION("Parentheses") {
@@ -207,9 +207,9 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(prod2.size() == 2);
     REQUIRE(prod2.scalar() == rational{-1, 2});
     REQUIRE(prod2.factor(0)->is<Product>());
-    REQUIRE(*prod2.factor(0)->at(0) == *parse_expr(L"g{i2,i3; a2,a3}"));
-    REQUIRE(*prod2.factor(0)->at(1) == *parse_expr(L"t{a1,a3; i2,i3}"));
-    REQUIRE(*prod2.factor(1) == *parse_expr(L"t{a2;i1}"));
+    REQUIRE(prod2.factor(0)->at(0) == parse_expr(L"g{i2,i3; a2,a3}"));
+    REQUIRE(prod2.factor(0)->at(1) == parse_expr(L"t{a1,a3; i2,i3}"));
+    REQUIRE(prod2.factor(1) == parse_expr(L"t{a2;i1}"));
 
     auto expr3 = parse_expr(
         L"(-1/2) ( g{i2,i3; a2,a3} * t{a1,a3; i2,i3} ) * (1/2) * ((t{a2;i1}))");
@@ -219,9 +219,9 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(prod3.size() == 2);
     REQUIRE(prod3.scalar() == rational{-1, 4});
     REQUIRE(prod3.factor(0)->is<Product>());
-    REQUIRE(*prod3.factor(0)->at(0) == *parse_expr(L"g{i2,i3; a2,a3}"));
-    REQUIRE(*prod3.factor(0)->at(1) == *parse_expr(L"t{a1,a3; i2,i3}"));
-    REQUIRE(*prod3.factor(1) == *parse_expr(L"t{a2;i1}"));
+    REQUIRE(prod3.factor(0)->at(0) == parse_expr(L"g{i2,i3; a2,a3}"));
+    REQUIRE(prod3.factor(0)->at(1) == parse_expr(L"t{a1,a3; i2,i3}"));
+    REQUIRE(prod3.factor(1) == parse_expr(L"t{a2;i1}"));
 
     auto expr4 = parse_expr(L"1/2 (a + b) * c");
     REQUIRE(expr4->is<Product>());
@@ -252,22 +252,22 @@ TEST_CASE("TEST_PARSE_EXPR", "[parse_expr]") {
     REQUIRE(sum.summand(0)->is<Product>());
     REQUIRE(sum.summand(0)->as<Product>().scalar() == rational{1, 4});
     REQUIRE(sum.summand(0)->size() == 1);
-    REQUIRE(*sum.summand(0)->at(0) == *parse_expr(L"g{a1,a2; i1,i2}"));
+    REQUIRE(sum.summand(0)->at(0) == parse_expr(L"g{a1,a2; i1,i2}"));
 
     REQUIRE(sum.summand(1)->is<Product>());
     auto const& prod = sum.summand(1)->as<Product>();
 
     REQUIRE(prod.scalar() == rational{1, 4});
     REQUIRE(prod.size() == 3);
-    REQUIRE(*prod.factor(0) == *parse_expr(L"g{i3,i4; a3,a4}"));
+    REQUIRE(prod.factor(0) == parse_expr(L"g{i3,i4; a3,a4}"));
 
     REQUIRE(prod.factor(1)->is<Product>());
-    REQUIRE(*prod.factor(1)->at(0) == *parse_expr(L"t{a3;i1}"));
-    REQUIRE(*prod.factor(1)->at(1) == *parse_expr(L"t{a4;i2}"));
+    REQUIRE(prod.factor(1)->at(0) == parse_expr(L"t{a3;i1}"));
+    REQUIRE(prod.factor(1)->at(1) == parse_expr(L"t{a4;i2}"));
 
     REQUIRE(prod.factor(2)->is<Product>());
-    REQUIRE(*prod.factor(2)->at(0) == *parse_expr(L"t{a1;i3}"));
-    REQUIRE(*prod.factor(2)->at(1) == *parse_expr(L"t{a2;i4}"));
+    REQUIRE(prod.factor(2)->at(0) == parse_expr(L"t{a1;i3}"));
+    REQUIRE(prod.factor(2)->at(1) == parse_expr(L"t{a2;i4}"));
   }
 
   SECTION("Empty input") { REQUIRE(parse_expr(L"") == nullptr); }
