974f3bc299cd617a2a8a9838ed162fd9cf930221
Robert Adam <dev@robert-adam.de>
2024-02-12 09:25:36 +0100

    Add options to TN graph creator

--- examples/tensor_network_graphs/tensor_network_graphs.cpp
+++ examples/tensor_network_graphs/tensor_network_graphs.cpp
@@ -35,13 +35,38 @@ std::optional<TensorNetwork> to_network(const ExprPtr &expr) {
 	}
 }
 
+void print_help() {
+	std::wcout << "Helper to generate dot (GraphViz) representations of tensor network graphs.\n";
+	std::wcout << "Usage:\n";
+	std::wcout << "  <exe> [options] <network 1> [<network 2> [... [<network N>] ] ]\n";
+	std::wcout << "Options:\n";
+	std::wcout << "  --help     Shows this help message\n";
+	std::wcout << "  --no-named Treat all indices as unnamed (even if they are external)\n";
+}
+
 int main(int argc, char **argv) {
 
 	set_locale();
 	mbpt::set_default_convention();
 
+	bool use_named_indices = true;
+	const TensorNetwork::named_indices_t empty_named_indices;
+
+	if (argc <= 1) {
+		print_help();
+		return 1;
+	}
+
 	for (std::size_t i = 1; i < static_cast<std::size_t>(argc); ++i) {
 		std::wstring current = from_utf8(argv[i]);
+		if (current == L"--help") {
+			print_help();
+			return 0;
+		} else if (current == L"--no-named") {
+			use_named_indices = false;
+			continue;
+		}
+
 		ExprPtr expr;
 		try {
 			expr = parse_expr(current);
@@ -57,7 +82,7 @@ int main(int argc, char **argv) {
 			return 2;
 		}
 
-		TensorNetwork::Graph graph = network->create_graph();
+		TensorNetwork::Graph graph = network->create_graph(use_named_indices ? nullptr : &empty_named_indices);
 		std::wcout << "Graph for '" << current << "'\n";
 		graph.bliss_graph->write_dot(std::wcout, graph.vertex_labels);
 	}
