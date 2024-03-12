707c1dd633611bb5844fb36c62c111ce2f26fe4f
Eduard Valeyev <eduard@valeyev.net>
2024-03-12 11:31:59 -0400

    introduced is_{,w}string_or_view_v

--- SeQuant/core/utility/string.hpp
+++ SeQuant/core/utility/string.hpp
@@ -14,19 +14,26 @@ namespace sequant {
 namespace meta {
 
 template <typename T>
-constexpr inline bool is_string_convertible_v =
+constexpr inline bool is_string_or_view_v =
     std::is_same_v<remove_cvref_t<T>, std::string> ||
-    std::is_same_v<remove_cvref_t<T>, std::string_view> ||
-    std::is_same_v<remove_cvref_t<T>, char[]> ||
+    std::is_same_v<remove_cvref_t<T>, std::string_view>;
+
+template <typename T>
+constexpr inline bool is_wstring_or_view_v =
+    std::is_same_v<remove_cvref_t<T>, std::wstring> ||
+    std::is_same_v<remove_cvref_t<T>, std::wstring_view>;
+
+template <typename T>
+constexpr inline bool is_string_convertible_v =
+    is_string_or_view_v<T> || std::is_same_v<remove_cvref_t<T>, char[]> ||
     std::is_same_v<remove_cvref_t<T>, const char[]> ||
     std::is_same_v<remove_cvref_t<T>, char *> ||
     std::is_same_v<remove_cvref_t<T>, const char *> ||
     std::is_same_v<remove_cvref_t<T>, char>;
+
 template <typename T>
 constexpr inline bool is_wstring_convertible_v =
-    std::is_same_v<remove_cvref_t<T>, std::wstring> ||
-    std::is_same_v<remove_cvref_t<T>, std::wstring_view> ||
-    std::is_same_v<remove_cvref_t<T>, wchar_t[]> ||
+    is_wstring_or_view_v<T> || std::is_same_v<remove_cvref_t<T>, wchar_t[]> ||
     std::is_same_v<remove_cvref_t<T>, const wchar_t[]> ||
     std::is_same_v<remove_cvref_t<T>, wchar_t *> ||
     std::is_same_v<remove_cvref_t<T>, const wchar_t *> ||
