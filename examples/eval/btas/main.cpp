ddbf1d59763bc0fbddcae060f64ffb475ee4f74f
connermasteran <cmasteran1@vt.edu>
2024-05-01 20:32:47 -0400

    refactor does not update examples

--- examples/eval/btas/main.cpp
+++ examples/eval/btas/main.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
   using namespace sequant;
   detail::OpIdRegistrar op_id_registrar;
   sequant::set_default_context(Context(
-      Vacuum::SingleProduct, mbpt::make_minimal_single_reference_subspaces(),
+      Vacuum::SingleProduct, mbpt::make_min_sr_subspaces(),
       IndexSpaceMetric::Unit, BraKetSymmetry::conjugate, SPBasis::spinorbital));
   TensorCanonicalizer::register_instance(
       std::make_shared<DefaultTensorCanonicalizer>());
