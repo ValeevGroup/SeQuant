31de37007b6a8407b84289c8d14937e85f7a78b4
Eduard Valeyev <eduard@valeyev.net>
2024-05-29 14:29:49 -0400

    Tensor::to_wolfram removed

--- SeQuant/core/tensor.hpp
+++ SeQuant/core/tensor.hpp
@@ -208,24 +208,6 @@ class Tensor : public Expr, public AbstractTensor, public Labeled {
     return result;
   }
 
-  /*std::wstring to_wolfram() const override {
-    std::wstring result;
-    result = L"SQM[OHead[\"\\!\\(\\*OverscriptBox[\\(";
-    result += this->label();
-    result += L"\\), \\(_\\)]\\)\",";
-    result += sequant::to_wolfram(this->symmetry());
-    result += L"],";
-    for (const auto &i : this->ket()) {
-      result += i.to_wolfram(BraKetPos::ket) + L",";
-    }
-    for (const auto &i : this->bra()) {
-      result += i.to_wolfram(BraKetPos::bra) + L",";
-    }
-    result = result.erase(result.size() - 1);
-    result += L"]";
-    return result;
-  }*/
-
   ExprPtr canonicalize() override;
 
   /// @brief adjoint of a Tensor swaps its bra and ket
