54e2f6c1a167bc654e8b321fc96e9aeae110c609
Ajay <ajaymk22@vt.edu>
2024-06-27 14:29:44 -0400

    Semifunctional Sphinx documentation generation

---
+++ doc/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SeQuant'
+copyright = '2024, Valeev Research Group'
+author = 'Valeev Research Group'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.inheritance_diagram',
+    'breathe',
+    "sphinx.ext.graphviz",
+    "myst_parser"
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+latex_engine = 'lualatex'
+
+breathe_default_project = "SeQuant"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+Welcome to SeQuant's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Introduction:
+
+   ...
+   rst/include_readme
+   ...
+
+.. toctree::
+    :maxdepth: 2
+
+    ...
+    rst/include_install
+    ...
+
+.. doxygenclass:: sequant::Context
+   :members:
