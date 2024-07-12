07502a86a44affcbe963401b6f732896a1d1c907
Ajay <ajaymk22@vt.edu>
2024-07-11 22:00:33 -0400

    Add html theme options in `conf.py`

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -54,3 +54,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
+
+html_theme_options = {
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links": True,
+    "vcs_pageview_mode": "",
+    # Toc options
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
