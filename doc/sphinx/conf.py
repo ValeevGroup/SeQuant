84941c9ac1c2ba789383869b04c7b58c0338ad91
Ajay <ajaymk22@vt.edu>
2024-07-13 19:19:48 -0400

    Modify the theme with CSS

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "/external/*"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 html_theme_options = {
     "display_version": True,
