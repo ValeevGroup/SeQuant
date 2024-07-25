4fb75060c733737c8c258e75b03daebe67454607
Eduard Valeyev <eduard@valeyev.net>
2024-07-25 10:47:59 -0400

    strong_type_base: dox + more comparisons out of the box

--- SeQuant/core/utility/strong.hpp
+++ SeQuant/core/utility/strong.hpp
@@ -9,9 +9,32 @@
 
 namespace sequant::detail {
 
+/// \brief Base class for strong types
+
+/// This class is modeled after the `strong_type` class of the excellent
+/// [type_safe](https://type_safe.foonathan.net) library, but with
+/// a stripped-down design covering our intended use cases. Namely, this class:
+/// - provides implicit cast to the underlying type to make many uses less
+/// verbose
+///   once the API surface has been breached
+/// - provides comparability and max/min functions if the underlying type
+/// supports them
+/// - provides range semantics if the underlying type supports it
+///
+/// Normally this is used as the base for the strong type, with the
+/// CRPT-like use of the strong type as \p Tag:
+/// \code
+/// struct StrongInt : strong_type_base<int, StrongInt> {
+///   using strong_type_base::strong_type_base;
+/// };
+/// \endcode
+/// \tparam T the underlying type
+/// \tparam Tag tag type
 template <typename T, typename Tag>
 class strong_type_base {
  public:
+  /// \name standard constructors and assignment operators
+  /// @{
   constexpr strong_type_base() noexcept(
       std::is_nothrow_default_constructible_v<T>) = default;
   constexpr strong_type_base(const strong_type_base&) noexcept(
@@ -22,23 +45,38 @@ class strong_type_base {
       std::is_nothrow_copy_assignable_v<T>) = default;
   constexpr strong_type_base& operator=(strong_type_base&&) noexcept(
       std::is_nothrow_move_assignable_v<T>) = default;
+  /// @}
 
+  /// \name constructors that take the value of underlying type
+  /// @{
+
+  /// \param value the value to initialize the strong type with
   explicit constexpr strong_type_base(const T& value) : value_(value) {}
 
+  /// \param value the value to initialize the strong type with
   explicit constexpr strong_type_base(T&& value) noexcept(
       std::is_nothrow_move_constructible<T>::value)
       : value_(std::move(value)) {}
+  /// @}
 
+  /// \name implicit conversion operators provide direct access the value
+  /// @{
   constexpr operator T&() & noexcept { return value_; }
   constexpr operator const T&() const& noexcept { return value_; }
   constexpr operator T&&() && noexcept { return std::move(value_); }
   constexpr operator const T&&() const&& noexcept { return std::move(value_); }
+  /// @}
 
+  /// \name explicit access to the value
+  /// @{
   constexpr T& value() & noexcept { return value_; }
   constexpr const T& value() const& noexcept { return value_; }
   constexpr T&& value() && noexcept { return std::move(value_); }
   constexpr const T&& value() const&& noexcept { return std::move(value_); }
+  /// @}
 
+  /// \name pass-through the range interface of \p T , if available
+  /// @{
   template <typename T_ = T,
             typename = std::enable_if_t<meta::has_memfn_size_v<const T_>>>
   decltype(auto) size() const {
@@ -67,29 +105,57 @@ class strong_type_base {
     using ranges::end;
     return end(value_);
   }
+  /// @}
 
   friend void swap(strong_type_base& a, strong_type_base& b) noexcept {
     using std::swap;
     swap(static_cast<T&>(a), static_cast<T&>(b));
   }
 
+  /// \name pass-through the comparison interface of \p T , if available
+  /// @{
   template <typename U, typename TagU,
-            typename = meta::are_less_than_comparable<T, U>>
-  friend std::common_type_t<T, U> const max(
-      const strong_type_base& a, const strong_type_base<U, TagU>& b) noexcept {
-    return static_cast<const T&>(a) < static_cast<const U&>(b)
-               ? static_cast<const U&>(b)
-               : static_cast<const T&>(a);
+            typename = std::enable_if_t<meta::are_equality_comparable_v<T, U>>>
+  friend bool operator==(const strong_type_base& a,
+                         const strong_type_base<U, TagU>& b) noexcept {
+    return static_cast<const T&>(a) == static_cast<const U&>(b);
+  }
+
+  template <typename U, typename TagU,
+            typename = std::enable_if_t<meta::are_less_than_comparable_v<T, U>>>
+  friend bool operator<(const strong_type_base& a,
+                        const strong_type_base<U, TagU>& b) noexcept {
+    return static_cast<const T&>(a) < static_cast<const U&>(b);
+  }
+
+  template <
+      typename U, typename TagU,
+      typename = std::enable_if_t<meta::are_greater_than_comparable_v<T, U> ||
+                                  (meta::are_equality_comparable_v<T, U> &&
+                                   meta::are_less_than_comparable_v<T, U>)>>
+  friend bool operator>(const strong_type_base& a,
+                        const strong_type_base<U, TagU>& b) noexcept {
+    if constexpr (meta::are_greater_than_comparable_v<T, U>)
+      return static_cast<const T&>(a) > static_cast<const U&>(b);
+    else
+      return !(static_cast<const T&>(a) < static_cast<const U&>(b) ||
+               static_cast<const T&>(a) == static_cast<const U&>(b));
   }
 
   template <typename U, typename TagU,
             typename = meta::are_less_than_comparable<T, U>>
-  friend std::common_type_t<T, U> const min(
+  friend std::common_type_t<T, U> max(
       const strong_type_base& a, const strong_type_base<U, TagU>& b) noexcept {
-    return static_cast<const T&>(a) < static_cast<const U&>(b)
-               ? static_cast<const T&>(a)
-               : static_cast<const U&>(b);
+    return a < b ? b.value() : a.value();
   }
+
+  template <typename U, typename TagU,
+            typename = meta::are_less_than_comparable<T, U>>
+  friend std::common_type_t<T, U> min(
+      const strong_type_base& a, const strong_type_base<U, TagU>& b) noexcept {
+    return a < b ? a.value() : b.value();
+  }
+  /// @}
 
  private:
   T value_{};
