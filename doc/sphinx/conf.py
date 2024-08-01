718d82e413c01666f84a82cde25cedd7efabb9b1
Ajay <ajaymk22@vt.edu>
2024-08-01 02:22:11 -0400

    Drop Exhale and Cleanup conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -1,40 +1,61 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SeQuant'
 copyright = '2024, Valeev Research Group'
 author = 'Valeev Research Group'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# General Configuration
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "/external/*"]
+# templates_path = ['_templates']
 
+html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+
+# Custom options for sphinx_rtd_theme
+html_theme_options = {
+    "display_version": True,
+    "vcs_pageview_mode": "",
+    # Toc options
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+}
+
+# To include GitHub repo info
+html_context = {
+    "display_github": True,
+    "github_user": "ajay-mk",
+    "github_repo": "SeQuant",
+    "github_version": "master",
+    "conf_py_path": "/doc/sphinx/"
+}
+
+# Primary Code Language
+primary_domain = "cpp"
+# Use xeLatex for support for unicode characters
+latex_engine = 'xelatex'
+
+# Extensions 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.inheritance_diagram',
+    "sphinx_copybutton",
     'breathe',
-    "sphinx.ext.graphviz",
-    "myst_parser",
-    "exhale"
+    "myst_parser"
 ]
 
-exhale_args = {
-    "containmentFolder": "./api",
-    "rootFileName": "library_root.rst",
-    "doxygenStripFromPath": "..",
-    "rootFileTitle": "Library API",
-    "createTreeView": False,
-}
+# Breathe Configuration
+# We use breathe to generate documentation from Doxygen XML files
+breath_projects = {"SeQuant": "./../doxygen/xml"}
+breathe_default_project = "SeQuant"
 
-primary_domain = "cpp"
+# Current default in Sphinx is MathJax v3, but we are using v4
+# see https://stackoverflow.com/questions/78813688/mathjax-rendering-issue-in-sphinx-with-symbol
+mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@4.0.0-beta.3/tex-mml-chtml.js'
 
+
+# MyST Enables parsing of markdown files
 myst_enable_extensions = [
     "amsmath",
     "attrs_inline",
@@ -50,39 +71,3 @@ myst_enable_extensions = [
     "substitution",
     "tasklist",
 ]
-
-latex_engine = 'lualatex'
-
-breathe_default_project = "SeQuant"
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "/external/*"]
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
-html_theme_options = {
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": False,
-    "vcs_pageview_mode": "",
-    # Toc options
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-}
-
-html_context = {
-    "display_github": True,
-    "github_user": "ajay-mk",
-    "github_repo": "SeQuant",
-    "github_version": "master",
-    "conf_py_path": "/doc/sphinx/"
-}
