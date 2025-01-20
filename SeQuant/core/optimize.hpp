b4aba3ac47be0aa797eb926430ea29aafd8c1c9d
Bimal Gaudel <bimalgaudel@gmail.com>
2025-01-20 14:25:15 -0500

    Handle non-tensors appearing as factors in single-term optimization of a product.

--- SeQuant/core/optimize.hpp
+++ SeQuant/core/optimize.hpp
@@ -353,18 +353,54 @@ template <typename IdxToSize,
 ExprPtr optimize(ExprPtr const& expr, IdxToSize const& idx2size,
                  bool reorder_sum) {
   using ranges::views::transform;
-  if (expr->is<Tensor>())
-    return expr->clone();
-  else if (expr->is<Product>())
-    return opt::single_term_opt(expr->as<Product>(), idx2size);
-  else if (expr->is<Sum>()) {
+  if (expr->is<Product>()) {
+    if (ranges::all_of(*expr, [](auto&& x) {
+          return x->template is<Tensor>() || x->template is<Variable>();
+        }))
+      return opt::single_term_opt(expr->as<Product>(), idx2size);
+    else {
+      auto const& prod = expr->as<Product>();
+
+      container::svector<ExprPtr> non_tensors(prod.size());
+      container::svector<ExprPtr> new_factors;
+
+      for (auto i = 0; i < prod.size(); ++i) {
+        auto&& f = prod.factor(i);
+        if (f.is<Tensor>() || f.is<Variable>())
+          new_factors.emplace_back(f);
+        else {
+          non_tensors[i] = f;
+          auto target_idxs = get_unique_indices(f);
+          new_factors.emplace_back(
+              ex<Tensor>(L"I_" + std::to_wstring(i), bra(target_idxs.bra),
+                         ket(target_idxs.ket), aux(target_idxs.aux)));
+        }
+      }
+
+      auto result = opt::single_term_opt(
+          Product(prod.scalar(), new_factors, Product::Flatten::No), idx2size);
+
+      auto replacer = [&non_tensors](ExprPtr& out) {
+        if (!out->is<Tensor>()) return;
+        auto const& tnsr = out->as<Tensor>();
+        auto&& label = tnsr.label();
+        if (label.at(0) == L'I' && label.at(1) == L'_') {
+          size_t suffix = std::stoi(std::wstring(label.data() + 2));
+          out = non_tensors[suffix].clone();
+        }
+      };
+
+      result->visit(replacer, /* atoms_only = */ true);
+      return result;
+    }
+  } else if (expr->is<Sum>()) {
     auto smands = *expr | transform([&idx2size](auto&& s) {
       return optimize(s, idx2size);
     }) | ranges::to_vector;
     auto sum = Sum{smands.begin(), smands.end()};
     return reorder_sum ? ex<Sum>(opt::reorder(sum)) : ex<Sum>(std::move(sum));
   } else
-    throw std::runtime_error{"Optimization attempted on unsupported Expr type"};
+    return expr->clone();
 }
 
 }  // namespace opt
