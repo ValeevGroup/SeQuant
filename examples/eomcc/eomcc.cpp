b6f0aac6729203e08811b1728ce047747a116cf6
Ajay <ajaymk22@vt.edu>
2025-02-04 01:48:29 -0500

    Introduce `eomcc` target which can derive eom equations
    Runs, need to add assertions

---
+++ examples/eomcc/eomcc.cpp
@@ -0,0 +1,182 @@
+//
+// Created by Ajay Melekamburath on 2/3/25.
+//
+#include <SeQuant/core/runtime.hpp>
+#include <SeQuant/core/tensor_canonicalizer.hpp>
+#include <SeQuant/core/timer.hpp>
+#include <SeQuant/domain/mbpt/context.hpp>
+#include <SeQuant/domain/mbpt/convention.hpp>
+#include <SeQuant/domain/mbpt/models/cc.hpp>
+
+using namespace sequant;
+using namespace sequant::mbpt;
+
+namespace {
+// TODO: Implement runtime_assert
+
+TimerPool<32> timer_pool;
+
+std::pair<int64_t, int64_t> parse_excitation_manifold(std::string& str) {
+  std::pair<int64_t, int64_t> result;
+
+  ranges::transform(str, str.begin(), ::tolower);
+  const auto h_pos = str.find('h');
+  const auto p_pos = str.find('p');
+
+  if (h_pos == std::string::npos && p_pos == std::string::npos) {
+    throw std::runtime_error(
+        "Invalid excitation manifold string: must contain 'h' or 'p'");
+  }
+
+  if (h_pos != std::string::npos && p_pos == std::string::npos) {
+    // holes only
+    result.first = std::stoi(str.substr(0, h_pos));
+    result.second = 0;
+  } else if (p_pos != std::string::npos && h_pos == std::string::npos) {
+    // particles only
+    result.first = 0;
+    result.second = std::stoi(str.substr(0, p_pos));
+  } else {
+    // hp/ph cases
+    if (h_pos < p_pos) {
+      result.first = std::stoi(str.substr(0, h_pos));
+      result.second = std::stoi(str.substr(h_pos + 1, p_pos - h_pos - 1));
+    } else {
+      result.first = std::stoi(str.substr(p_pos + 1, h_pos - p_pos - 1));
+      result.second = std::stoi(str.substr(0, p_pos));
+    }
+  }
+
+  if (result.first == 0 && result.second == 0)
+    throw std::runtime_error(
+        "Invalid excitation manifold: both particle and hole ranks cannot be "
+        "zero");
+
+  return result;
+}
+
+enum class EqnType { left, right };
+
+inline const container::map<std::string, EqnType> str2type = {
+    {"L", EqnType::left}, {"R", EqnType::right}};
+
+inline const container::map<EqnType, std::wstring> type2wstr = {
+    {EqnType::left, L"L"}, {EqnType::right, L"R"}};
+
+class compute_eomcc {
+  size_t N;
+  std::string manifold;
+  EqnType type;
+
+ public:
+  compute_eomcc(size_t n, const std::string& exc_manifold,
+                EqnType t = EqnType::right)
+      : N(n), manifold(exc_manifold), type(t) {}
+
+  void operator()(bool print) {
+    assert(get_default_context().spbasis() == SPBasis::spinorbital);
+    const auto [nh, np] = parse_excitation_manifold(manifold);
+    timer_pool.start(N);
+    std::vector<ExprPtr> eqvec;
+    switch (type) {
+      case EqnType::right:
+        eqvec = CC{N}.eom_r(nₚ(np), nₕ(nh));
+        break;
+      case EqnType::left:
+        eqvec = CC{N}.eom_l(nₚ(np), nₕ(nh));
+        break;
+    }
+    timer_pool.stop(N);
+
+    std::wcout << std::boolalpha
+               << "EOM-CC Equations [type=" << type2wstr.at(type)
+               << ", CC rank=" << N
+               << ", manifold=" << sequant::to_wstring(manifold) << "]"
+               << " computed in " << timer_pool.read(N) << " s\n";
+    size_t idx = 0;
+    for (const auto& eq : eqvec) {
+      if (eq == nullptr) {
+        idx++;
+        continue;
+      }
+      std::wcout << "R[" << idx << "] has " << eq->size() << " terms\n";
+      if (print) std::wcout << to_latex_align(eq, 20, 1) << "\n";
+
+      // TODO: add assertions here
+      idx++;
+    }
+  }
+};  // class compute_eomcc
+
+class compute_all {
+  size_t NMAX;
+  std::string manifold;
+  EqnType type;
+
+ public:
+  compute_all(size_t nmax, const std::string manifold,
+              EqnType t = EqnType::right)
+      : NMAX(nmax), manifold(manifold), type(t) {}
+
+  void operator()(bool print = false) {
+    for (size_t N = 1; N <= NMAX; ++N) {
+      std::vector<std::string> manifold_vec;
+      auto [Nh, Np] = parse_excitation_manifold(manifold);
+      // generate all possible manifolds
+      while (Nh >= 0 && Np >= 0) {
+        if (Nh == 0 && Np == 0) break;
+        manifold_vec.push_back(std::to_string(Nh) + "h" + std::to_string(Np) +
+                               "p");
+        if (Nh == 0 || Np == 0) break;
+        Nh--;
+        Np--;
+      }
+      for (auto it = manifold_vec.rbegin(); it != manifold_vec.rend(); ++it) {
+        compute_eomcc{N, *it, type}(print);
+      }
+    }
+  }
+};  // class compute_all
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  std::setlocale(LC_ALL, "en_US.UTF-8");
+  std::wcout.precision(std::numeric_limits<double>::max_digits10);
+  std::wcerr.precision(std::numeric_limits<double>::max_digits10);
+  std::wcout.sync_with_stdio(false);
+  std::wcerr.sync_with_stdio(false);
+  std::wcout.imbue(std::locale("en_US.UTF-8"));
+  std::wcerr.imbue(std::locale("en_US.UTF-8"));
+  std::wcout.sync_with_stdio(true);
+  std::wcerr.sync_with_stdio(true);
+
+#ifndef NDEBUG
+  constexpr size_t DEFAULT_NMAX = 3;
+#else
+  constexpr size_t DEFAULT_NMAX = 4;
+#endif
+
+  // read command line arguments
+  const size_t NMAX = argc > 1 ? std::atoi(argv[1]) : DEFAULT_NMAX;
+  assert(NMAX > 0 && "Invalid NMAX");
+  const std::string exc_manifold =
+      argc > 2 ? argv[2]
+               : (std::to_string(NMAX) + "h" + std::to_string(NMAX) + "p");
+  assert(!exc_manifold.empty() && "Invalid excitation manifold");
+  const std::string eqn_type = argc > 3 ? argv[3] : "right";
+  const std::string print_str = argc > 4 ? argv[4] : "noprint";
+  const bool print = print_str == "print";
+
+  sequant::detail::OpIdRegistrar op_id_registrar;
+  sequant::set_default_context(sequant::Context(
+      make_min_sr_spaces(), Vacuum::SingleProduct, IndexSpaceMetric::Unit,
+      BraKetSymmetry::conjugate, SPBasis::spinorbital));
+  TensorCanonicalizer::register_instance(
+      std::make_shared<DefaultTensorCanonicalizer>());
+
+  // change to true to print stats
+  Logger::instance().wick_stats = false;
+
+  // call the compute_all function here
+  compute_all{NMAX, exc_manifold, str2type.at(eqn_type)}(print);
+}
