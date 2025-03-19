60415c0cf0baa0272f12205854abf4b3cdb8dae3
Robert Adam <dev@robert-adam.de>
2025-03-19 15:27:39 +0100

    Update libPerm version

--- cmake/modules/FindOrFetchLibPerm.cmake
+++ cmake/modules/FindOrFetchLibPerm.cmake
@@ -1,7 +1,7 @@
 if (NOT TARGET libperm)
 
     include(${vg_cmake_kit_SOURCE_DIR}/modules/VRGFindOrFetchPackage.cmake)
-    VRGFindOrFetchPackage(libperm "https://github.com/Krzmbrzl/libPerm.git" "v1.5.0"
+    VRGFindOrFetchPackage(libperm "https://github.com/Krzmbrzl/libPerm.git" "v1.8.0"
             ADD_SUBDIR
             CONFIG_SUBDIR
     )
