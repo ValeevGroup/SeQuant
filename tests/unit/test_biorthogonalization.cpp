1bb961a34658ca8092c0210c9095187a245104ff
Robert Adam <dev@robert-adam.de>
2025-03-23 17:44:39 +0100

    Add test case for triples result

--- tests/unit/test_biorthogonalization.cpp
+++ tests/unit/test_biorthogonalization.cpp
@@ -16,11 +16,19 @@ TEST_CASE("biorthogonalization", "[Biorthogonalization]") {
 
   SECTION("plain ExprPtr") {
     const std::vector<std::wstring> inputs = {
-        L"t{a1;i1}", L"S{i1,i2;a1,a2}:S g{a1,a2;i1,i2}"};
+        L"t{a1;i1}", L"S{i1,i2;a1,a2}:S g{a1,a2;i1,i2}",
+        L"S{a1,a2,a3;i1,i2,i3}:S t{i1,i2,i3;a1,a2,a3}"};
     const std::vector<std::wstring> expected_outputs = {
         L"1/2 t{a1;i1}",
         L"S{i1,i2;a1,a2}:S 1/6 (2 g{a1,a2;i1,i2} + g{a2,a1;i1,i2})",
-    };
+        // TODO: this is not an ideal test case as this simply sums to zero
+        L"S{a1,a2,a3;i1,i2,i3}:S 1/20 ("
+        "-7 t{i_1,i_2,i_3;a_2,a_3,a_1}:N-C-S "
+        "- 7 t{i_1,i_2,i_3;a_3,a_1,a_2}:N-C-S "
+        "- t{i_1,i_2,i_3;a_2,a_1,a_3}:N-C-S "
+        "- t{i_1,i_2,i_3;a_3,a_2,a_1}:N-C-S "
+        "- t{i_1,i_2,i_3;a_1,a_3,a_2}:N-C-S "
+        "+ 17t{i_1,i_2,i_3;a_1,a_2,a_3}:N-C-S)"};
 
     REQUIRE(inputs.size() == expected_outputs.size());
 
