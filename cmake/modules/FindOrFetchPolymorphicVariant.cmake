85888d246e5759a8db8bb77aa0558737b80ff651
Robert Adam <dev@robert-adam.de>
2025-05-12 14:57:16 +0200

    Add polymorphic_variant as a dependency

---
+++ cmake/modules/FindOrFetchPolymorphicVariant.cmake
@@ -0,0 +1,13 @@
+if (NOT TARGET libperm)
+    include(${vg_cmake_kit_SOURCE_DIR}/modules/VRGFindOrFetchPackage.cmake)
+	VRGFindOrFetchPackage(polymorphic_variant "https://github.com/Krzmbrzl/polymorphic_variant.git" "${SEQUANT_TRACKED_POLYMORPHICVARIANT_TAG}"
+            ADD_SUBDIR
+			ADD_SUBDIR_EXCLUDE_FROM_ALL
+            CONFIG_SUBDIR
+    )
+endif()
+
+# postcond check
+if (NOT TARGET polymorphic_variant::polymorphic_variant)
+    message(FATAL_ERROR "FindOrFetchLibPerm could not make TARGET polymorphic_variant::polymorphic_variant available")
+endif(NOT TARGET libperm)
