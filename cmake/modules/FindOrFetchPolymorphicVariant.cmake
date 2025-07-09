f54ba2bdf25f7b7c1bdef37916fae9def9b515fd
Robert Adam <dev@robert-adam.de>
2025-07-09 12:32:15 +0200

    Fix dependency declaration

--- cmake/modules/FindOrFetchPolymorphicVariant.cmake
+++ cmake/modules/FindOrFetchPolymorphicVariant.cmake
@@ -1,4 +1,4 @@
-if (NOT TARGET libperm)
+if (NOT TARGET polymorphic_variant::polymorphic_variant)
     include(${vg_cmake_kit_SOURCE_DIR}/modules/VRGFindOrFetchPackage.cmake)
 	VRGFindOrFetchPackage(polymorphic_variant "https://github.com/Krzmbrzl/polymorphic_variant.git" "${SEQUANT_TRACKED_POLYMORPHICVARIANT_TAG}"
             ADD_SUBDIR
@@ -10,4 +10,4 @@ endif()
 # postcond check
 if (NOT TARGET polymorphic_variant::polymorphic_variant)
     message(FATAL_ERROR "FindOrFetchLibPerm could not make TARGET polymorphic_variant::polymorphic_variant available")
-endif(NOT TARGET libperm)
+endif()
