6f8b56d7024702fdf74939b63e7b8ee089bbe928
Robert Adam <dev@robert-adam.de>
2025-08-27 10:33:14 +0200

    Format Python-related files

--- python/include/sequant-python/boost/container/small_vector.h
+++ python/include/sequant-python/boost/container/small_vector.h
@@ -7,9 +7,9 @@
 PYBIND11_NAMESPACE_BEGIN(PYBIND11_NAMESPACE)
 PYBIND11_NAMESPACE_BEGIN(detail)
 
-  template <typename T, std::size_t N>
-  struct type_caster< boost::container::small_vector<T,N> >
-    : list_caster< boost::container::small_vector<T,N>, T > { };
+template <typename T, std::size_t N>
+struct type_caster<boost::container::small_vector<T, N> >
+    : list_caster<boost::container::small_vector<T, N>, T> {};
 
 PYBIND11_NAMESPACE_END(detail)
 PYBIND11_NAMESPACE_END(PYBIND11_NAMESPACE)
