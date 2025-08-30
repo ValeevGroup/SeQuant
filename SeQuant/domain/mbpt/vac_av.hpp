dc11c22edcd03c5eb63183e6af0c8fde58780938
Ajay <ajaymk22@vt.edu>
2025-08-30 08:13:04 +0200

    clang-format: fix formatting

--- SeQuant/domain/mbpt/vac_av.hpp
+++ SeQuant/domain/mbpt/vac_av.hpp
@@ -85,13 +85,16 @@ inline ExprPtr lower_to_tensor_form(const ExprPtr& expr_inp) {
 /// `OpType::h` will be connected to at least one `OpType::t` on its right
 /// (preceding it). The default list of connections is given
 /// by default_op_connections() .
-/// @param[in] use_topology if true, WickTheorem uses topological equivalence of terms
-/// @param[in] screen if true, expressions are screened before calling WickTheorem
+/// @param[in] use_topology if true, WickTheorem uses topological equivalence of
+/// terms
+/// @param[in] screen if true, expressions are screened before calling
+/// WickTheorem
 /// @param[in] skip_clone if true, will not clone the input expression
 /// @return the VEV
 ExprPtr vac_av(ExprPtr expr,
                const OpConnections<mbpt::OpType>& op_connections =
-                   default_op_connections(), bool use_topology = true, bool screen = true,
+                   default_op_connections(),
+               bool use_topology = true, bool screen = true,
                bool skip_clone = false);
 
 /// computes the vacuum expectation value (VEV)
@@ -101,8 +104,10 @@ ExprPtr vac_av(ExprPtr expr,
 /// connected; connections are defined left-to-right, i.e., pair
 /// `{opL,opR}` declares that `opL` and `opR` are to be connected
 /// when `opR` precedes `opL`, i.e. `opL` is to the left of `opR`
-/// @param[in] use_topology if true, WickTheorem uses topological equivalence of terms
-/// @param[in] screen if true, expressions are screened before calling WickTheorem
+/// @param[in] use_topology if true, WickTheorem uses topological equivalence of
+/// terms
+/// @param[in] screen if true, expressions are screened before calling
+/// WickTheorem
 /// @param[in] skip_clone if true, will not clone the input expression
 /// @return the VEV
 ExprPtr vac_av(ExprPtr expr, const OpConnections<std::wstring>& op_connections,
