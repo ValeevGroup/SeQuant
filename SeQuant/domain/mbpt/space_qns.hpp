a995ccd11271cd8b808f64e3980f55f42e529709
Eduard Valeyev <eduard@valeyev.net>
2025-09-02 17:33:08 -0400

    [dox] typo

--- SeQuant/domain/mbpt/space_qns.hpp
+++ SeQuant/domain/mbpt/space_qns.hpp
@@ -49,7 +49,7 @@ struct mask<Spin> {
 };
 
 /// quantum numbers tags related to LCAO basis traits
-/// \note sLCAO basis traits use 3rd and 4th rightmost bits
+/// \note LCAO basis traits use 3rd and 4th rightmost bits
 enum class LCAOQNS : bitset_t {
   ao = 0b000100,
   pao = 0b001000,  // projected AO space denotes unoccupied spaces made from AO
