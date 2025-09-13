76d6368bd542771eaf4c946ccacc3fa76f0d484e
Azam Besharat <besharat@vt.edu>
2025-09-13 18:58:48 -0400

    set the renamed functions

--- tests/integration/stcc_v1.cpp
+++ tests/integration/stcc_v1.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]) {
   std::vector<ExprPtr> cc_st_r(cc_r.size());
   for (auto i = 1; i < cc_r.size(); ++i) {
     const auto tstart = std::chrono::high_resolution_clock::now();
-    cc_st_r[i] = sequant::closed_shell_CC_spintrace_optimal(cc_r[i]);
+    cc_st_r[i] = sequant::closed_shell_CC_v1_spintrace(cc_r[i]);
 
     auto tstop = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time_elapsed = tstop - tstart;
