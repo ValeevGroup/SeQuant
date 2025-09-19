c55203e3e95602a9fe4771d0c7a3f94106d8a745
Ajay <ajaymk22@vt.edu>
2025-09-19 17:02:33 -0400

    unit: revert 48d5e46842bed8ba34730cf2e3645cb8ff76450f

--- tests/unit/test_mbpt.cpp
+++ tests/unit/test_mbpt.cpp
@@ -464,9 +464,10 @@ TEST_CASE("mbpt", "[mbpt]") {
       REQUIRE(to_latex(sum1) == L"{{{2}}{\\hat{h¹}}{[{z}_{1}]}}");
       auto sum2 = h1 + pt1;
       simplify(sum2);
-      std::wcout << "sum2:  " << to_latex(sum2) << std::endl;
-      // REQUIRE(to_latex(sum2) == L"{ \\bigl({\\hat{h¹}}{[{z}_{1}]} +
-      // {\\hat{t¹}_{2}}{[{z}_{1}]} + {\\hat{t¹}_{1}}{[{z}_{1}]}\\bigr) }");
+      // std::wcout << "sum2:  " << to_latex(sum2) << std::endl;
+      REQUIRE(to_latex(sum2) ==
+              L"{ \\bigl({\\hat{h¹}}{[{z}_{1}]} + {\\hat{t¹}_{2}}{[{z}_{1}]} + "
+              L"{\\hat{t¹}_{1}}{[{z}_{1}]}\\bigr) }");
 
       // TODO: Confirm if the products have the intended behavior
       auto pdt1 = h1 * h1_2;
