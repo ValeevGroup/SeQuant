f9f6912eac5b612ad12f3444c4418b3b3ded57bc
Ajay <ajaymk22@vt.edu>
2025-09-26 12:53:38 -0400

    dox: cleanup

--- SeQuant/domain/mbpt/convention.cpp
+++ SeQuant/domain/mbpt/convention.cpp
@@ -100,13 +100,11 @@ void add_pao_spaces(std::shared_ptr<IndexSpaceRegistry>& isr) {
 
 void add_df_spaces(std::shared_ptr<IndexSpaceRegistry>& isr) {
   // matches the MPQC layout, see spindex.h
-  isr->add(IndexSpace{L"Κ", 0b00001, TensorFactorizationQNS::df})  // DFBS AO
-      ;
+  isr->add(IndexSpace{L"Κ", 0b00001, TensorFactorizationQNS::df});  // DFBS AO
 }
 
 void add_batching_spaces(std::shared_ptr<IndexSpaceRegistry>& isr) {
-  // z will the index registered for the batch space
-  isr->add(IndexSpace{L"z", 0b000001, BatchingQNS::batch});
+  isr->add(IndexSpace{L"z", 0b000001, BatchingQNS::batch});  // Batching Space
 }
 
 std::shared_ptr<IndexSpaceRegistry> make_min_sr_spaces(SpinConvention spconv) {
