9518f2356c6ad45e8b067adf718e14fe5dc4d9ba
Ajay <ajaymk22@vt.edu>
2025-11-17 11:41:43 -0500

    CC: update pt method calls according to e2ab4f7279039e5dc3be005f231813a7096fe37c
    Validated numerically in MPQC.

--- SeQuant/domain/mbpt/models/cc.cpp
+++ SeQuant/domain/mbpt/models/cc.cpp
@@ -152,13 +152,12 @@ std::vector<ExprPtr> CC::t_pt(size_t rank, [[maybe_unused]] size_t order,
   // truncate h1_bar at rank 2 for one-body perturbation
   // operator and at rank 4 for two-body perturbation operator
   const auto h1_truncate_at = rank == 1 ? 2 : 4;
-  const auto h1_bar =
-      mbpt::lst(H_pt({.rank = rank, .order = order, .nbatch = nbatch}), T(N),
-                h1_truncate_at);
+  const auto h1_bar = mbpt::lst(H_pt(rank, {.order = order, .nbatch = nbatch}),
+                                T(N), h1_truncate_at);
 
   // construct [hbar, T(1)]
-  const auto hbar_pert = mbpt::lst(H(), T(N), 3) *
-                         T_pt({.rank = N, .order = order, .nbatch = nbatch});
+  const auto hbar_pert =
+      mbpt::lst(H(), T(N), 3) * T_pt(N, {.order = order, .nbatch = nbatch});
 
   // [Eq. 34, WIREs Comput Mol Sci. 2019; 9:e1406]
   const auto expr = simplify(h1_bar + hbar_pert);
@@ -176,7 +175,7 @@ std::vector<ExprPtr> CC::t_pt(size_t rank, [[maybe_unused]] size_t order,
   std::vector<ExprPtr> result(N + 1);
   for (auto p = N; p >= 1; --p) {
     const auto freq_term = ex<Variable>(L"ω") * P(nₚ(p)) *
-                           T_pt_({.rank = p, .order = order, .nbatch = nbatch});
+                           T_pt_(p, {.order = order, .nbatch = nbatch});
     result.at(p) =
         this->ref_av(P(nₚ(p)) * expr, op_connect) - this->ref_av(freq_term);
   }
@@ -200,19 +199,18 @@ std::vector<ExprPtr> CC::λ_pt(size_t rank, [[maybe_unused]] size_t order,
   // truncate h1_bar at rank 2 for one-body perturbation
   // operator and at rank 4 for two-body perturbation operator
   const auto h1_truncate_at = rank == 1 ? 2 : 4;
-  const auto h1_bar =
-      mbpt::lst(H_pt({.rank = rank, .order = order, .nbatch = nbatch}), T(N),
-                h1_truncate_at);
+  const auto h1_bar = mbpt::lst(H_pt(rank, {.order = order, .nbatch = nbatch}),
+                                T(N), h1_truncate_at);
 
   // construct [hbar, T(1)]
-  const auto hbar_pert = mbpt::lst(H(), T(N), 3) *
-                         T_pt({.rank = N, .order = order, .nbatch = nbatch});
+  const auto hbar_pert =
+      mbpt::lst(H(), T(N), 3) * T_pt(N, {.order = order, .nbatch = nbatch});
 
   // [Eq. 35, WIREs Comput Mol Sci. 2019; 9:e1406]
   const auto One = ex<Constant>(1);
   const auto expr =
       simplify((One + Λ(N)) * (h1_bar + hbar_pert) +
-               Λ_pt({.rank = N, .order = order, .nbatch = nbatch}) * hbar);
+               Λ_pt(N, {.order = order, .nbatch = nbatch}) * hbar);
 
   // connectivity:
   // t and t1 with {h,f,g}
@@ -236,9 +234,9 @@ std::vector<ExprPtr> CC::λ_pt(size_t rank, [[maybe_unused]] size_t order,
 
   std::vector<ExprPtr> result(N + 1);
   for (auto p = N; p >= 1; --p) {
-    const auto freq_term =
-        ex<Variable>(L"ω") *
-        Λ_pt_({.rank = p, .order = order, .nbatch = nbatch}) * P(nₚ(-p));
+    const auto freq_term = ex<Variable>(L"ω") *
+                           Λ_pt_(p, {.order = order, .nbatch = nbatch}) *
+                           P(nₚ(-p));
     result.at(p) =
         this->ref_av(expr * P(nₚ(-p)), op_connect) + this->ref_av(freq_term);
   }
