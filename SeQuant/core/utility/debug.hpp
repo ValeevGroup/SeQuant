94ccc45d11e5125ced3f5b52146d18cd145cab52
Eduard Valeyev <eduard@valeyev.net>
2025-11-21 16:54:28 -0500

    introduced sequant::printf

--- SeQuant/core/utility/debug.hpp
+++ SeQuant/core/utility/debug.hpp
@@ -13,10 +13,19 @@ namespace sequant {
 /// uses std::wostringstream + std::wprintf to print to stdout even if wcout is
 /// captured (e.g., by Catch2)
 template <typename... Args>
+void printf(Args&&... args) {
+  std::ostringstream oss;
+  (oss << ... << std::forward<Args>(args));
+  std::printf("%s", oss.str().c_str());
+}
+
+/// uses std::wostringstream + std::wprintf to print to stdout even if wcout is
+/// captured (e.g., by Catch2)
+template <typename... Args>
 void wprintf(Args&&... args) {
   std::wostringstream oss;
   (oss << ... << std::forward<Args>(args));
-  std::wprintf(oss.str().c_str());
+  std::wprintf(L"%sl", oss.str().c_str());
 }
 
 }  // namespace sequant
