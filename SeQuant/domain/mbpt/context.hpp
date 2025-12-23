6b3952feffe3a97f4a395a641594b0b51c7e3c5d
Ajay <ajaymk22@vt.edu>
2025-12-23 00:27:32 -0500

    feat: introduce `mbpt::Context::Options`

--- SeQuant/domain/mbpt/context.hpp
+++ SeQuant/domain/mbpt/context.hpp
@@ -5,6 +5,7 @@
 #include <SeQuant/domain/mbpt/fwd.hpp>
 
 #include <SeQuant/core/utility/context.hpp>
+#include <SeQuant/domain/mbpt/op_registry.hpp>
 
 namespace sequant::mbpt {
 
@@ -22,6 +23,20 @@ class Context {
 
   /// @brief Construct a Formalism object
   explicit Context(CSV csv_formalism = Defaults::csv) noexcept;
+  struct Options {
+    /// shared pointer to operator registry
+    std::shared_ptr<OpRegistry> op_registry_ptr = nullptr;
+    /// optional operator registry, use if no shared pointer is provided
+    std::optional<OpRegistry> op_registry = std::nullopt;
+    /// whether to use cluster-specific virtuals
+    CSV csv = Defaults::csv;
+  };
+
+  /// @brief makes default options for mbpt::Context
+  static Options make_default_options() {
+    return Options{};
+  }
+
 
   CSV csv() const { return csv_; }
 
