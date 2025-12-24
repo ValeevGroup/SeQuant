3ab85e5728c5808a1f5a80add826c7cec991d727
Ajay <ajaymk22@vt.edu>
2025-12-24 12:18:05 -0500

    refactor: change initialization order in `mbpt::Context::Options`

--- SeQuant/domain/mbpt/context.hpp
+++ SeQuant/domain/mbpt/context.hpp
@@ -22,12 +22,12 @@ class Context {
   };
 
   struct Options {
+    /// whether to use cluster-specific virtuals
+    CSV csv = Defaults::csv;
     /// shared pointer to operator registry
     std::shared_ptr<OpRegistry> op_registry_ptr = nullptr;
     /// optional operator registry, use if no shared pointer is provided
     std::optional<OpRegistry> op_registry = std::nullopt;
-    /// whether to use cluster-specific virtuals
-    CSV csv = Defaults::csv;
   };
 
   /// @brief makes default options for mbpt::Context
