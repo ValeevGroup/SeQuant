c3e7dbeb50594b5fc5f8c05068378cdb85e06914
Eduard Valeyev <eduard@valeyev.net>
2026-02-05 09:44:21 -0500

    assert_failed + abort_msg moved to macros.cpp

---
+++ SeQuant/core/utility/macros.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Eduard Valeyev on 7/31/23
+//
+
+#include <SeQuant/core/utility/macros.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+#define SEQUANT_ASSERT_BEHAVIOR \
+  SEQUANT_CONCAT(SEQUANT_ASSERT_, SEQUANT_ASSERT_BEHAVIOR_)
+
+namespace sequant {
+
+#ifdef SEQUANT_ASSERT_ENABLED
+void assert_failed(const std::string &errmsg,
+                   const std::source_location location) {
+#if SEQUANT_ASSERT_BEHAVIOR == SEQUANT_ASSERT_THROW
+  std::ostringstream oss;
+  oss << errmsg << " at " << location.file_name() << ":" << location.line()
+      << " in function '" << location.function_name() << "'";
+  throw sequant::Exception(oss.str());
+#elif SEQUANT_ASSERT_BEHAVIOR == SEQUANT_ASSERT_ABORT
+  std::cerr << errmsg << " at " << location.file_name() << ":"
+            << location.line() << " in function '" << location.function_name()
+            << "'" << std::endl;
+  std::abort();
+#endif
+}
+#else
+void assert_failed(const std::string &, const std::source_location) {}
+#endif
+
+[[noreturn]] void abort_msg(const std::string &errmsg,
+                            const std::source_location location) {
+  std::cerr << errmsg << " at " << location.file_name() << ":"
+            << location.line() << " in function '" << location.function_name()
+            << "'";
+  std::abort();
+}
+
+}  // namespace sequant
