76cb07b3bc2cf84f543cfeb5c16c64633e3620d0
Robert Adam <dev@robert-adam.de>
2026-07-24 16:22:22 +0200

    Implement ValidateStep

---
+++ utilities/external-interface/validate_step.hpp
@@ -0,0 +1,28 @@
+#ifndef SEQUANT_EXTERNAL_INTERFACE_VALIDATESTEP_HPP
+#define SEQUANT_EXTERNAL_INTERFACE_VALIDATESTEP_HPP
+
+#include "processing_step.hpp"
+
+#include <nlohmann/json_fwd.hpp>
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace sequant::util::extint {
+
+class ValidateStep : public ProcessingStep {
+ public:
+  std::string kind() const override;
+
+  bool accepts_options() const override;
+  bool requires_options() const override;
+  void set_options(const nlohmann::json &options) override;
+
+  std::size_t run(std::string_view step_id, ExecutionContext &ctx,
+                  const std::vector<std::string_view> &inputs = {}) override;
+};
+
+}  // namespace sequant::util::extint
+
+#endif  // SEQUANT_EXTERNAL_INTERFACE_VALIDATESTEP_HPP
