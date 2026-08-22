3e798cb1bcb254bc9fab7eb98b9fb6069e638e29
Robert Adam <dev@robert-adam.de>
2026-08-22 10:07:17 +0200

    Make benchmarks work properly with OpRegistry
    PR #456 introduced operator registries but forgot to update the
    benchmark codes. This commit ensures that were needed we now set up a
    default registry to ensure the benchmarks can run as before.

--- benchmarks/wick.cpp
+++ benchmarks/wick.cpp
@@ -148,7 +148,8 @@ VacAvPair get_mbpt_expr(std::size_t i) {
 
 static void mbpt_vac_av(benchmark::State &state, bool csv) {
   auto ctx = sequant::mbpt::set_scoped_default_mbpt_context(
-      mbpt::Context({.csv = csv ? CSV::Yes : CSV::No}));
+      mbpt::Context({.csv = csv ? CSV::Yes : CSV::No,
+                     .op_registry_ptr = make_minimal_registry()}));
 
   VacAvPair input = get_mbpt_expr(state.range(0));
 
