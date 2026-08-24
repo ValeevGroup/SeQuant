e976a19e4568a22d39e53aa82fdd456463047a55
Robert Adam <dev@robert-adam.de>
2026-08-24 15:35:39 +0200

    Move ctx setup to main

--- benchmarks/coupled_cluster.cpp
+++ benchmarks/coupled_cluster.cpp
@@ -1,6 +1,5 @@
 #include <benchmark/benchmark.h>
 
-#include <SeQuant/domain/mbpt/context.hpp>
 #include <SeQuant/domain/mbpt/models/cc.hpp>
 
 #ifdef SEQUANT_BENCH_MAX_CC_RANK
@@ -14,8 +13,6 @@ using namespace sequant::mbpt;
 
 static void cc_full_derivation(benchmark::State &state) {
   const std::size_t rank = state.range(0);
-
-  set_default_mbpt_context({.op_registry_ptr = make_minimal_registry()});
 
   for (auto _ : state) {
     CC cc(rank);
