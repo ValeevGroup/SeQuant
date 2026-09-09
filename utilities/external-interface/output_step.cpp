2c364fc0d537dd9d1a79d62e85102f977e519a4a
Robert Adam <dev@robert-adam.de>
2026-09-09 11:58:01 +0200

    Fix casing of kind()

--- utilities/external-interface/output_step.cpp
+++ utilities/external-interface/output_step.cpp
@@ -15,7 +15,7 @@ namespace sequant::util::extint {
 
 SEQUANT_EXTINT_REGISTER_STEP_TYPE(OutputStep);
 
-std::string OutputStep::kind() const { return "Output"; }
+std::string OutputStep::kind() const { return "output"; }
 
 bool OutputStep::accepts_options() const { return true; }
 
