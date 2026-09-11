0ce2e32233f5b00f576090bcffc705991c700031
Kshitij Surjuse <kshitijsurjuse100@gmail.com>
2026-09-11 13:14:54 -0400

    eval: the node-range evaluate overload forwards its Trace parameter (the energy was never traced)
    (cherry picked from commit 0dc38f15f09fc6f309ef66fd3b625a02bf765e2c)

--- SeQuant/core/eval/eval.hpp
+++ SeQuant/core/eval/eval.hpp
@@ -903,7 +903,7 @@ ResultPtr evaluate(Nodes const& nodes,  //
   });
 
   static_assert(std::is_default_constructible_v<annot_type>);
-  return evaluate(nodes, annot_type{}, le, cache);
+  return evaluate<EvalTrace>(nodes, annot_type{}, le, cache);
 }
 
 ///
